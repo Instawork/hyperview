e7f6bdbf5dd9ea7c746de6e7fe3804638470b75f
flochtililoch <flochtililoch@gmail.com>
2018-11-28 17:30:38 -0800

    Fix templates generator script

--- scripts/generate/templates.js
+++ scripts/generate/templates.js
@@ -2,11 +2,12 @@
 
 import * as Files from './files';
 import glob from 'glob';
+import packageJson from '../../package.json';
 import path from 'path';
 
 const rootDir = path.join(__dirname, '../..');
 const targetFilePath = path.join(rootDir, 'storybook/templates.gen.js');
-const projectName = path.basename(rootDir);
+const projectName = packageJson.name;
 
 const templates = glob
   .sync(path.join(rootDir, 'src/**/stories/*.xml'))
