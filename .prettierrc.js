f1a2af8ac2f6d45cb8949fe8be0d1b86f524cf5e
Adam Stepinski <adam@instawork.com>
2019-02-05 21:25:38 -0800

    fix prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   trailingComma: 'all',
   singleQuote: true,
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 };
