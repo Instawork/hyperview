f1edc00189cc44340759603133977ae22e678911
flochtililoch <flochtililoch@gmail.com>
2019-03-13 17:09:26 -0700

    Tools: `yarn build:demo` -> `yarn sync [path]` Rename `build-demo` script to `sync`, and add ability to specify the path where to perform sync.

--- scripts/sync/index.js
+++ scripts/sync/index.js
@@ -6,9 +6,11 @@ const pathModule = require('path');
 
 const PROJECT_DIR = pathModule.join(__dirname, '../..');
 const SRC_DIR = pathModule.join(PROJECT_DIR, 'src');
+const DEST_PROJECT_REL_PATH = process.argv[2] || './demo';
 const HYPERVIEW_DIR = pathModule.join(
   PROJECT_DIR,
-  'demo/node_modules/hyperview',
+  DEST_PROJECT_REL_PATH,
+  '/node_modules/hyperview',
 );
 
 const updatePackageJson = () => {
@@ -18,7 +20,7 @@ const updatePackageJson = () => {
 };
 
 const syncFiles = (event, path = SRC_DIR) => {
-  console.log(`Syncing ${path}…`);
+  console.log(`Syncing ${path} with ${HYPERVIEW_DIR}…`);
   const cmd = `rsync -v -r --delete ${path} ${HYPERVIEW_DIR}`;
   childProcess.execSync(cmd);
 };
