4c614b4848d0bfba9b71375413ec0888de22603f
Adam Stepinski <adam@instawork.com>
2019-05-18 18:40:19 -0700

    get all xml files

--- scripts/format-xml.sh
+++ scripts/format-xml.sh
@@ -1,5 +1,5 @@
 find examples -type f -name '*.xml' -exec sed -i "" 's/id=/aaaaa=/g' {} +
 
-/usr/local/bin/tidy -config tidy.cfg -m examples/*.xml examples/**/*.xml
+find examples -type f -name '*.xml' | xargs /usr/local/bin/tidy -config tidy.cfg -m
 
 find examples -type f -name '*.xml' -exec sed -i "" 's/aaaaa=/id=/g' {} +
