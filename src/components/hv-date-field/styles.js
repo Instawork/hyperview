de47d47d7cfdce7c474df72c628b227e5fb21f97
Adam Stepinski <adam@instawork.com>
2019-05-30 07:16:25 -0700

    prototyping
    render given value
    
    dep injection
    
    wip
    
    update expo
    
    get example working
    
    add min and max date
    
    add comments

---
+++ src/components/hv-date-field/styles.js
@@ -0,0 +1,22 @@
+// @flow
+
+/**
+ * Copyright (c) Garuda Labs, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  modalWrapper: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  modalActions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
