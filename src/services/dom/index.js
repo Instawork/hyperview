94d90db7ced002cf7e24503cd7f1fdad4c83c450
Ashwini Batrahalli <ashwini@instawork.com>
2020-05-13 10:31:01 +0530

    added test validate function

--- src/services/dom/index.js
+++ src/services/dom/index.js
@@ -17,9 +17,8 @@ import { Dimensions } from 'react-native';
 import type { Document } from 'hyperview/src/types';
 import { version } from 'hyperview/package.json';
 
-const assert = require('assert');
-const crypto = require('crypto');
-const stream = require('stream');
+import { XSD } from 'hyperview/schema';
+const xmllint = require('xmllint');
 
 const { width, height } = Dimensions.get('window');
 const headers = {
@@ -42,6 +41,19 @@ const parser = new DOMParser({
     },
   },
 });
+
+function test_validate() {
+  const xsd =
+    '<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"><xs:element name="comment"><xs:complexType><xs:all><xs:element name="author" type="xs:string"/><xs:element name="content" type="xs:string"/></xs:all></xs:complexType></xs:element></xs:schema>';
+  const xml_valid =
+    '<?xml version="1.0"?><comment><author>author</author><content>nothing</content></comment>';
+  const xml_invalid = '<?xml version="1.0"?><comment>A comment</comment>';
+
+  const valid_result = xmllint.validateXML({ xml: xml_valid, schema: xsd });
+  const invalid_result = xmllint.validateXML({ xml: xml_invalid, schema: xsd });
+
+  console.log('\nINVALID DOC', invalid_result);
+}
 
 export class Parser {
   fetch: Fetch;
@@ -80,14 +92,7 @@ export class Parser {
     const response: Response = await this.fetch(url, options);
     const responseText: string = await response.text();
 
-    const alice = crypto.createDiffieHellman(256);
-    const bob = crypto.createDiffieHellman(
-      alice.getPrime(),
-      alice.getGenerator(),
-    );
-
-    const alice_key = alice.generateKeys();
-    const bob_key = bob.generateKeys();
+    test_validate();
 
     if (this.onBeforeParse) {
       this.onBeforeParse(url);
