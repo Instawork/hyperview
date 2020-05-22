75b0052da482f6f8fed6bcc5dae0f7550bd48dbb
Adam Stepinski <adam@instawork.com>
2020-05-22 15:45:13 -0700

    fixes

--- website/build-schema.sh
+++ website/build-schema.sh
@@ -3,5 +3,6 @@
 rm -rf build/schema
 mkdir build/schema
 cp ../schema/*.xsd build/schema
+# Replace local schema location references with production references.
 sed -i .bak 's/schemaLocation="/schemaLocation="https:\/\/hyperview.org\/schema\//g' build/schema/*.xsd
 rm build/schema/*.bak
