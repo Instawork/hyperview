56487c121a20c1cee014fa0ec0e95955b4ad96ca
Adam Stepinski <adam@instawork.com>
2020-05-26 09:31:31 -0700

    replace location in all files

--- website/build-schema.sh
+++ website/build-schema.sh
@@ -3,4 +3,4 @@
 mkdir build/Hyperview/schema
 cp ../schema/*.xsd build/Hyperview/schema
 # Replace local schema location references with production references.
-sed -i 's/schemaLocation=\"/schemaLocation=\"https:\/\/hyperview.org\/schema\//g' build/Hyperview/schema/hyperview.xsd
+sed -i 's/schemaLocation=\"/schemaLocation=\"https:\/\/hyperview.org\/schema\//g' build/Hyperview/schema/*.xsd
