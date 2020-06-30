1d296046a69cd73ef201e40e62f0d0e19f680d4e
flochtililoch <flochtililoch@gmail.com>
2020-07-01 01:18:35 +0530

    Fix date picker Generate dates in local timezone instead of UTC, so that formatting remains consistent. Also update demo app Hyperview version.
    https://app.asana.com/0/244065166232575/1182609012386243/f

--- src/components/hv-date-field/index.js
+++ src/components/hv-date-field/index.js
@@ -76,9 +76,9 @@ export default class HvDateField extends PureComponent<
     if (!date) {
       return '';
     }
-    const year = date.getUTCFullYear();
-    const month = date.getUTCMonth() + 1;
-    const day = date.getUTCDate();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
     return `${year}-${month}-${day}`;
   };
 
