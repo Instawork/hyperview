e0ead69beb2ea9c8031cc0ebd425e0fd9ed05551
Adam Stepinski <adam@instawork.com>
2020-08-27 12:50:17 -0700

    Set test props for <picker-field> on Android (#181)
    * set test props for <picker-field> on Android
    
    * Update src/services/index.js
    
    Co-authored-by: Florent Bonomo <flochtililoch+github@gmail.com>
    
    * lint
    
    Co-authored-by: Florent Bonomo <flochtililoch+github@gmail.com>

--- src/services/index.test.js
+++ src/services/index.test.js
@@ -8,7 +8,19 @@
  *
  */
 
-import { encodeXml } from '.';
+import { createProps, createTestProps, encodeXml } from '.';
+import { DOMParser } from 'xmldom-instawork';
+import { createStylesheets } from 'hyperview/src/services/stylesheets';
+
+const parser = new DOMParser();
+const createElement = (id: ?string) => {
+  const doc = parser.parseFromString('<doc></doc>');
+  const el = doc.createElement('<el>');
+  if (id) {
+    el.setAttribute('id', id);
+  }
+  return el;
+};
 
 describe('encodeXml', () => {
   it('encodes XML entities in an XML string', () => {
@@ -18,6 +30,55 @@ describe('encodeXml', () => {
       ),
     ).toEqual(
       '&lt;behavior href=&quot;https://hyperview.org/foo=bar&amp;bar=baz&quot; action=&apos;new&apos; /&gt;',
+    );
+  });
+});
+
+describe('createTestProps', () => {
+  it('sets testID from id attribute', () => {
+    expect(createTestProps(createElement('myID'))).toHaveProperty(
+      'testID',
+      'myID',
+    );
+  });
+
+  it('sets accessibilityLabel from id attribute', () => {
+    expect(createTestProps(createElement('myID'))).toHaveProperty(
+      'accessibilityLabel',
+      'myID',
+    );
+  });
+
+  it('returns empty object if no id attribute present', () => {
+    expect(createTestProps(createElement(null))).toEqual({});
+  });
+
+  it('returns empty id attribute is empty', () => {
+    expect(createTestProps(createElement(''))).toEqual({});
+  });
+});
+
+describe('createProps', () => {
+  const styleSheets = createStylesheets(parser.parseFromString('<doc></doc>'));
+
+  it('sets id from id attribute', () => {
+    expect(createProps(createElement('myID'), styleSheets, {})).toHaveProperty(
+      'id',
+      'myID',
+    );
+  });
+
+  it('sets testID from id attribute', () => {
+    expect(createProps(createElement('myID'), styleSheets, {})).toHaveProperty(
+      'testID',
+      'myID',
+    );
+  });
+
+  it('sets accessibilityLabel from id attribute', () => {
+    expect(createProps(createElement('myID'), styleSheets, {})).toHaveProperty(
+      'accessibilityLabel',
+      'myID',
     );
   });
 });
