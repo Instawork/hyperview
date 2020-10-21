32a9ecc5b29ebac70646b8a71707a9aefd4580cc
ashwinibm <ashwinibm88@gmail.com>
2020-10-21 21:06:45 +0530

    Update src/components/hv-view/types.js
    Co-authored-by: Florent Bonomo <flochtililoch+github@gmail.com>

--- src/components/hv-view/types.js
+++ src/components/hv-view/types.js
@@ -12,7 +12,7 @@ import type { StyleSheet } from 'react-native/Libraries/StyleSheet/StyleSheetTyp
 
 export type InternalProps = {|
   accessibilityLabel?: ?string,
-  behavior?: ?string,
+  behavior?: 'position',
   extraScrollHeight?: ?number,
   keyboardOpeningTime?: ?number,
   keyboardShouldPersistTaps?: ?string,
