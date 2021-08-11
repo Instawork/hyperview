e57739f78cdfa44e903d51a63c1a33e5ed25fb8f
Adam Stepinski <adam@instawork.com>
2021-08-11 08:53:37 -0700

    removing promises

--- src/behaviors/hv-validate/index.js
+++ src/behaviors/hv-validate/index.js
@@ -11,6 +11,7 @@ import type {
   HvUpdateRoot,
   Node,
   Validator,
+  Validation,
   ValidatorRegistry,
 } from 'hyperview/src/types';
 import { NODE_TYPE } from 'hyperview/src/types';
@@ -21,32 +22,52 @@ const V_NS = "https://hyperview.org/hyperview-validation";
 const RequiredValidator: Validator = {
   namespace: V_NS,
   name: "required",
-  check: (value: ?string, element: Element): Promise<Validation> => (
-    new Promise((resolve, reject) => {
-      if (!!value) {
-        resolve({
-          valid: true,
-          message: "Field is valid"
-        });
-      } else {
-        reject({
+  check: (value: ?string, element: Element): Validation => {
+    if (!!value) {
+      return {
+        valid: true,
+      };
+    }
+    return {
+      valid: false,
+      message: element.getAttribute('message') || 'This field is required',
+    };
+  },
+};
+
+
+const LengthValidator: Validator = {
+  namespace: V_NS,
+  name: "length",
+  check: (value: ?string, element: Element): Validation => {
+
+    const minLength = parseInt(element.getAttribute('min-length'), 10);
+    const maxLength = parseInt(element.getAttribute('max-length'), 10);
+
+    if(value !== null) {
+      if (value.length < minLength || value.length > maxLength) {
+        return {
           valid: false,
-          message: "Field is required"
-        });
+          message: element.getAttribute('message') || 'This field has bad length',
+        }
       }
-    });
-  ),
+    }
+
+    return { valid: true };
+  },
 };
 
 const VALIDATORS = [
   RequiredValidator,
+  LengthValidator,
 ];
 
 const REGISTRY: ValidatorRegistry = {};
-VALIDATORS.each((v) => {
-  namespace = REGISTRY[v.namespace] || {};
-  namespace[v.name] = v;
-  REGISTRY[v.namespace] = namespace;
+
+VALIDATORS.forEach((v) => {
+  const namespaceDict = REGISTRY[v.namespace] || {};
+  namespaceDict[v.name] = v;
+  REGISTRY[v.namespace] = namespaceDict;
 });
 
 export default {
@@ -58,9 +79,21 @@ export default {
     updateRoot: HvUpdateRoot,
   ) => {
 
-    const validators: Array<Validator> = Array.from(element.childNodes).map((e) => {
-      const namespace = REGISTRY[e.namespaceURI] || {};
-      return namespace[e.tagName];
-    }).filter((v) => !!v);
+    const inputId: ?string = element.getAttribute("target");
+    const inputElement: Element = inputId ? getRoot().getElementById(inputId) : element;
+
+    const value: ?string = inputElement.getAttribute('value');
+
+    // Find validators for the element
+    const validators: Array<Validator> = Array.from(targetElement.childNodes)
+      .filter((n) => n.nodeType == NODE_TYPE.ELEMENT_NODE)
+      .map((e) => {
+        const namespace = REGISTRY[e.namespaceURI] || {};
+        const validator = namespace[e.localName];
+        if (validator) {
+          const result = validator.check(value, e);
+          console.log(result);
+        }
+      });
   }
 };
