d0b679ac569c30fbff0fa7af433f9f9bd5a0ce7e
flochtililoch <flochtililoch@gmail.com>
2022-02-08 16:59:53 -0800

    refactor: Core app - rewrite core app to work with react-navigation 6.x - copy over assets/delete unused assets

--- demo/App.js
+++ demo/App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Copyright (c) Garuda Labs, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import Navigator from './src/Navigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ demo/src/Navigator.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Garuda Labs, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import HyperviewScreen from './HyperviewScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ENTRY_POINT_URL, MAIN_STACK_NAME, MODAL_STACK_NAME } from './constants';
+
+
+const Stack = createStackNavigator();
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Group>
+          <Stack.Screen
+            component={HyperviewScreen}
+            initialParams={{ url: ENTRY_POINT_URL }}
+            name={MAIN_STACK_NAME}
+          />
+        </Stack.Group>
+        <Stack.Group screenOptions={{ presentation: 'modal' }}>
+          <Stack.Screen
+            component={HyperviewScreen}
+            name={MODAL_STACK_NAME}
+          />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
