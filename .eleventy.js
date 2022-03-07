49561d81b743d39b8958b83e2f6026957179743f
Adam Stepinski <adam@instawork.com>
2022-03-07 09:45:15 -0800

    clean up eleventy implementation

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
 module.exports = function (eleventyConfig) {
+  // Padd through any XML files that haven't been ported yet.
+  eleventyConfig.addPassthroughCopy("examples/**/*.xml");
+  eleventyConfig.addPassthroughCopy("examples/**/*.jpg");
+  eleventyConfig.addPassthroughCopy("examples/**/*.jpeg");
+  eleventyConfig.addPassthroughCopy("examples/**/*.png");
   return {
     dir: {
-      // default: [site root]
       input: "examples",
-      // default: _site
-      output: "public",
+      output: "_examples_site",
     },
   };
 };
