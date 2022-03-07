1ea21515fdc96b7465869e58b13f8d2095507e4c
Adam Stepinski <adam@instawork.com>
2022-03-06 22:21:35 -0800

    wip

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+  return {
+    dir: {
+      // default: [site root]
+      input: "examples",
+      // default: _site
+      output: "public",
+    },
+  };
+};
