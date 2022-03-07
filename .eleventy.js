cfe0278bc54173a06cf6651a186c8f776de367ad
Adam Stepinski <adam@instawork.com>
2022-03-07 10:07:10 -0800

    clean up

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
-  // Padd through any XML files that haven't been ported yet.
+  // Pass through any XML files that haven't been ported yet.
+  // Once everything is ported, we can remove this.
   eleventyConfig.addPassthroughCopy("examples/**/*.xml");
+  // Pass through images used by different screens.
   eleventyConfig.addPassthroughCopy("examples/**/*.jpg");
   eleventyConfig.addPassthroughCopy("examples/**/*.jpeg");
   eleventyConfig.addPassthroughCopy("examples/**/*.png");
