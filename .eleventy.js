fcbfe403e9a245838b1b6ac1071e7a183eea1f14
flochtililoch <flochtililoch@gmail.com>
2022-07-17 09:50:14 -0700

    fix: configure CORS

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,20 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setBrowserSyncConfig({
+    middleware: function (req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'x-hyperview-dimensions,x-hyperview-version,pragma,expires,cache-control',
+      );
+      if (req.method === 'OPTIONS') {
+        res.writeHead(200, {});
+        res.end();
+      } else {
+        next();
+      }
+    },
+  });
   // Pass through any XML files that haven't been ported yet.
   // Once everything is ported, we can remove this.
   eleventyConfig.addPassthroughCopy("examples/**/*.xml");
