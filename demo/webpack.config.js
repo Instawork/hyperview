2fb1bd7b2820e2063f4da444fb47f3e451657e64
flochtililoch <flochtililoch@gmail.com>
2022-09-29 10:35:16 +0200

    fix(demo): Transpile Hyperview - run command `expo customize:web` to generate `webpack.config.js` - add configuration to transpile hyperview library when bundling demo app
    This helps running the demo app on web

---
+++ demo/webpack.config.js
@@ -0,0 +1,11 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync({
+    ...env,
+    babel: {
+      dangerouslyAddModulePathsToTranspile: ['hyperview'],
+    },
+  }, argv);
+  return config;
+};
