3a564127a0f73e87a55f14653b61ec485269a8be
Hardin Gray <hgray@instawork.com>
2023-06-22 16:08:54 -0400

    Reworked logic

--- src/core/components/hv-route/index.tsx
+++ src/core/components/hv-route/index.tsx
@@ -363,16 +363,14 @@ export default function HvRoute(props: Types.Props) {
     props.route?.params?.id || navigatorContext.initialRouteName || undefined;
 
   const { index, type } = props.navigation?.getState() || {};
-  // The nested element is only used when there is an id
-  //    and the navigator is not a stack or is the first screen in a stack
-  //    other stack screens will use a url
-  const includeElement =
-    id && (type !== NavigatorService.NAVIGATOR_TYPE.STACK || index === 0);
+  // The nested element is only used when the navigator is not a stack
+  //    or is the first screen in a stack. Other stack screens will require a url
+  const includeElement: boolean =
+    type !== NavigatorService.NAVIGATOR_TYPE.STACK || index === 0;
 
   // Get the navigator element from the context
-  const element: TypesLegacy.Element | undefined = includeElement
-    ? navigatorContext.elementMap?.get(id)
-    : undefined;
+  const element: TypesLegacy.Element | undefined =
+    id && includeElement ? navigatorContext.elementMap?.get(id) : undefined;
 
   return (
     <HvRouteInner
