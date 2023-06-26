33cc26c03ba871da45aba432907aecf4a183ef3f
Hardin Gray <hgray@instawork.com>
2023-06-26 11:53:22 -0400

    Moved the tsx root under new 'hv-root', re-created src/index as a js to provide exports, ensure build includes ts/x files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// @flow
+
+/**
+ * Copyright (c) Garuda Labs, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import * as Events from 'hyperview/src/services/events';
+import * as Namespaces from 'hyperview/src/services/namespaces';
+import Hyperview from 'hyperview/lib/core/components/hv-root';
+
+export * from 'hyperview/src/types';
+export { Events, Hyperview, Namespaces };
