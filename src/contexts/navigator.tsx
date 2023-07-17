5ce99d5b09e0f57ddf9591a2595902dcf9b3763b
Hardin Gray <hgray@instawork.com>
2023-07-17 16:39:01 -0400

    Updated context to use getters/setters

--- src/contexts/navigator.tsx
+++ src/contexts/navigator.tsx
@@ -7,33 +7,81 @@
  */
 
 import * as TypesLegacy from 'hyperview/src/types-legacy';
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
-export type NavigatorContextProps = {
-  routeMap?: Map<string, string>;
-  elementMap?: Map<string, TypesLegacy.Element>;
+export type NavigatorMapContextProps = {
+  setRoute: (key: string, route: string) => void;
+  getRoute: (key: string) => string | undefined;
+  setElement: (key: string, element: TypesLegacy.Element) => void;
+  getElement: (key: string) => TypesLegacy.Element | undefined;
+  setPreload: (key: number, element: TypesLegacy.Element) => void;
+  getPreload: (key: number) => TypesLegacy.Element | undefined;
   initialRouteName?: string;
-  preloadMap?: Map<number, TypesLegacy.Element>;
 };
 
 /**
  * Context used to store runtime information about the navigator and urls
  * Each navigator creates its own context
- * Urls defined in <nav-route> elements are stored in the routeMap by their key
+ *  - routeMap: Urls defined in <nav-route> elements are stored in the routeMap by their key
+ *  - elementMap: Contains element sub-navigators defined in a <nav-route> element
+ *  - initialRouteName: The name of the first route to render
+ *  - preloadMap: A map of preload elements by their id
  */
-export const NavigatorMapContext = createContext<NavigatorContextProps>({});
+export const NavigatorMapContext = createContext<NavigatorMapContextProps>({
+  getElement: () => undefined,
+  getPreload: () => undefined,
+  getRoute: () => '',
+  setElement: () => undefined,
+  setPreload: () => undefined,
+  setRoute: () => undefined,
+});
 
 type Props = { children: React.ReactNode };
 
 /**
- * Encapsulated context provider
+ * Encapsulated context provider. The maps are intentionally not updating state; their purpose is to
+ * store runtime information about the navigator and urls.
  */
 export function NavigatorMapProvider(props: Props) {
-  const routeMap: Map<string, string> = new Map();
-  const elementMap: Map<string, TypesLegacy.Element> = new Map();
-  const preloadMap: Map<number, TypesLegacy.Element> = new Map();
+  const [routeMap] = useState<Map<string, string>>(new Map());
+  const [elementMap] = useState<Map<string, TypesLegacy.Element>>(new Map());
+  const [preloadMap] = useState<Map<number, TypesLegacy.Element>>(new Map());
+
+  const setRoute = (key: string, route: string) => {
+    routeMap.set(key, route);
+  };
+
+  const getRoute = (key: string): string | undefined => {
+    return routeMap.get(key);
+  };
+
+  const setElement = (key: string, element: TypesLegacy.Element) => {
+    elementMap.set(key, element);
+  };
+
+  const getElement = (key: string): TypesLegacy.Element | undefined => {
+    return elementMap.get(key);
+  };
+
+  const setPreload = (key: number, element: TypesLegacy.Element) => {
+    preloadMap.set(key, element);
+  };
+
+  const getPreload = (key: number): TypesLegacy.Element | undefined => {
+    return preloadMap.get(key);
+  };
+
   return (
-    <NavigatorMapContext.Provider value={{ elementMap, preloadMap, routeMap }}>
+    <NavigatorMapContext.Provider
+      value={{
+        getElement,
+        getPreload,
+        getRoute,
+        setElement,
+        setPreload,
+        setRoute,
+      }}
+    >
       {props.children}
     </NavigatorMapContext.Provider>
   );
