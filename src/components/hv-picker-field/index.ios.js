Instawork/hyperview

commit 25d1633ca92fd2d87a8c504a1b61245a50ab6a2f

refactor: Pickers UX (#633)
This PR brings UX and code improvement to the `date-field` and
`picker-field` components.

Integrates the following changes:
- #630 
- #634
- #635
- #636
- #638
- #647
- #648
- #652
- #654 

https://app.asana.com/0/1204008699308084/1205235979122187/f

|  | Before | After |
|--|---|---|
| iOS |
![ios-before](https://github.com/Instawork/hyperview/assets/309515/b1b21226-bc81-4ec1-b18d-03a612c63a61)
|
![after-ios](https://github.com/Instawork/hyperview/assets/309515/eba16e72-7fd1-45e7-8e76-c71f97b07c92)
|
| Android |
![android-before](https://github.com/Instawork/hyperview/assets/309515/bd735070-7cd9-4d17-b2d2-a42eed6f3bf8)
|
![android-after](https://github.com/Instawork/hyperview/assets/309515/b7f82416-98ed-482a-93a0-541c9d1fc795)
|











TODO:
- [x] fix android selection bug
- [x] animate overlay fade in / out
- [x] allow separate configuration for open vs close animation timings

---------

Co-authored-by: flochtililoch <flochtililoch@gmail.com>

src/components/hv-picker-field/index.ios.js
@@ -22,7 +22,9 @@ import {
   createTestProps,
   getNameValueFormInputValues,
 } from 'hyperview/src/services';
+import Field from './field';
 import { LOCAL_NAME } from 'hyperview/src/types';
+import Modal from 'hyperview/src/core/components/modal';
 import type { Node } from 'react';
 import Picker from 'hyperview/src/core/components/picker';
 import { View } from 'react-native';
@@ -44,15 +46,53 @@ export default class HvPickerField extends PureComponent<HvComponentProps> {
     return getNameValueFormInputValues(element);
   };
 
+  getPickerInitialValue = (): string => {
+    const value = this.getValue();
+    const pickerItems = this.getPickerItems();
+    if (pickerItems.map(item => item.getAttribute('value')).includes(value)) {
+      return value;
+    }
+    if (pickerItems.length > 0) {
+      return pickerItems[0].getAttribute('value') || '';
+    }
+    return '';
+  };
+
   /**
    * Returns a string representing the value in the field.
    */
   getValue = (): string => this.props.element.getAttribute('value') || '';
 
   /**
+   * Gets the label from the picker items for the given value.
+   * If the value doesn't have a picker item, returns null.
+   */
+  getLabelForValue = (value: DOMString): ?string => {
+    // $FlowFixMe: flow thinks `element` is a `Node` instead of an `Element`
+    const pickerItemElements: NodeList<Element> = this.props.element.getElementsByTagNameNS(
+      Namespaces.HYPERVIEW,
+      LOCAL_NAME.PICKER_ITEM,
+    );
+
+    let item: ?Element = null;
+    for (let i = 0; i < pickerItemElements.length; i += 1) {
+      const pickerItemElement: ?Element = pickerItemElements.item(i);
+      if (
+        pickerItemElement &&
+        pickerItemElement.getAttribute('value') === value
+      ) {
+        item = pickerItemElement;
+        break;
+      }
+    }
+    return item ? item.getAttribute('label') : null;
+  };
+
+  /**
    * Returns a string representing the value in the picker.
    */
-  getPickerValue = (): string => this.props.element.getAttribute('value') || '';
+  getPickerValue = (): string =>
+    this.props.element.getAttribute('picker-value') || '';
 
   getPickerItems = (): Element[] =>
     Array.from(
@@ -63,18 +103,15 @@ export default class HvPickerField extends PureComponent<HvComponentProps> {
       ),
     );
 
-  onFocus = () => {
+  /**
+   * Shows the picker, defaulting to the field's value. If the field is not set, use the first value in the picker.
+   */
+  onFieldPress = () => {
     const newElement = this.props.element.cloneNode(true);
     newElement.setAttribute('focused', 'true');
+    newElement.setAttribute('picker-value', this.getPickerInitialValue());
     this.props.onUpdate(null, 'swap', this.props.element, { newElement });
     Behaviors.trigger('focus', newElement, this.props.onUpdate);
-  };
-
-  onBlur = () => {
-    const newElement = this.props.element.cloneNode(true);
-    newElement.setAttribute('focused', 'false');
-    this.props.onUpdate(null, 'swap', this.props.element, { newElement });
-    Behaviors.trigger('blur', newElement, this.props.onUpdate);
   };
 
   /**
@@ -85,36 +122,43 @@ export default class HvPickerField extends PureComponent<HvComponentProps> {
     newElement.setAttribute('focused', 'false');
     newElement.removeAttribute('picker-value');
     this.props.onUpdate(null, 'swap', this.props.element, { newElement });
+    Behaviors.trigger('blur', newElement, this.props.onUpdate);
   };
 
   /**
    * Hides the picker and applies the chosen value to the field.
    */
-  onDone = (newValue?: string) => {
-    const pickerValue =
-      newValue !== undefined ? newValue : this.getPickerValue();
+  onDone = () => {
+    const pickerValue = this.getPickerValue();
     const value = this.getValue();
     const newElement = this.props.element.cloneNode(true);
     newElement.setAttribute('value', pickerValue);
     newElement.removeAttribute('picker-value');
     newElement.setAttribute('focused', 'false');
     this.props.onUpdate(null, 'swap', this.props.element, { newElement });
-
     const hasChanged = value !== pickerValue;
     if (hasChanged) {
       Behaviors.trigger('change', newElement, this.props.onUpdate);
     }
+    Behaviors.trigger('blur', newElement, this.props.onUpdate);
   };
 
-  render = (): Node => {
-    const onChange = (value: ?string) => {
-      if (value === undefined) {
-        this.onCancel();
-      } else {
-        this.onDone(value || '');
-      }
-    };
+  /**
+   * Updates the picker value while keeping the picker open.
+   */
+  setPickerValue = (value: string) => {
+    const newElement = this.props.element.cloneNode(true);
+    newElement.setAttribute('picker-value', value);
+    this.props.onUpdate(null, 'swap', this.props.element, { newElement });
+  };
 
+  /**
+   * Returns true if the field is focused (and picker is showing).
+   */
+  isFocused = (): boolean =>
+    this.props.element.getAttribute('focused') === 'true';
+
+  render = (): Node => {
     const style: Array<StyleSheet> = createStyleProp(
       this.props.element,
       this.props.stylesheets,
@@ -143,48 +187,52 @@ export default class HvPickerField extends PureComponent<HvComponentProps> {
 
     // Gets all of the <picker-item> elements. All picker item elements
     // with a value and label are turned into options for the picker.
-    const items = this.getPickerItems();
-    const children = items.filter(Boolean).map((item: Element) => {
-      const l: ?DOMString = item.getAttribute('label');
-      const v: ?DOMString = item.getAttribute('value');
-      if (!l || typeof v !== 'string') {
-        return null;
-      }
-      const enabled = ['', 'true'].includes(item.getAttribute('enabled'));
-      return <Picker.Item key={l + v} enabled={enabled} label={l} value={v} />;
-    });
-
-    // If there are no items, or the first item has a value,
-    // we need to add an empty option that acts as a placeholder.
-    if (items.length > 0 && items[0].getAttribute('value') !== '') {
-      children.unshift(
-        <Picker.Item
-          key="empty"
-          // `enabled` needs to be true when the field is not focused, otherwise the the field will not be selectable
-          // fix inspired by https://github.com/react-native-picker/picker/issues/95#issuecomment-935718568
-          enabled={this.props.element.getAttribute('focused') !== 'true'}
-          label={this.props.element.getAttribute('placeholder')}
-          value=""
-        />,
-      );
-    }
+    const children = this.getPickerItems()
+      .filter(Boolean)
+      .map((item: Element) => {
+        const l: ?DOMString = item.getAttribute('label');
+        const v: ?DOMString = item.getAttribute('value');
+        if (!l || typeof v !== 'string') {
+          return null;
+        }
+        return <Picker.Item key={l + v} label={l} value={v} />;
+      });
 
     return (
-      <View
-        accessibilityLabel={accessibilityLabel}
-        style={fieldStyle}
-        testID={testID}
+      <Field
+        element={this.props.element}
+        focused={this.isFocused()}
+        onPress={this.onFieldPress}
+        options={this.props.options}
+        stylesheets={this.props.stylesheets}
+        value={this.getLabelForValue(this.getValue())}
       >
-        <Picker
-          onBlur={this.onBlur}
-          onFocus={this.onFocus}
-          onValueChange={onChange}
-          selectedValue={this.getPickerValue()}
-          style={style}
-        >
-          {children}
-        </Picker>
-      </View>
+        {this.isFocused() ? (
+          <Modal
+            element={this.props.element}
+            isFocused={this.isFocused}
+            onModalCancel={this.onCancel}
+            onModalDone={this.onDone}
+            onUpdate={this.props.onUpdate}
+            options={this.props.options}
+            stylesheets={this.props.stylesheets}
+          >
+            <View
+              accessibilityLabel={accessibilityLabel}
+              style={fieldStyle}
+              testID={testID}
+            >
+              <Picker
+                onValueChange={this.setPickerValue}
+                selectedValue={this.getPickerValue()}
+                style={style}
+              >
+                {children}
+              </Picker>
+            </View>
+          </Modal>
+        ) : null}
+      </Field>
     );
   };
 }

examples/advanced_behaviors/set_value/index.xml.njk
@@ -75,6 +75,10 @@ tags: advanced_behaviors
         shadowOffsetY="-5"
         shadowOpacity="0.2"
         shadowRadius="5"/>
+      <style
+        id="PickerModal__overlay"
+        backgroundColor="#1f1f1fa0"
+      />
       <style id="PickerModal__text" color="blue" fontSize="16" fontWeight="600" marginBottom="16" padding="24">
         <modifier pressed="true">
           <style opacity="0.5"/>
@@ -249,6 +253,7 @@ tags: advanced_behaviors
             id="id_df"
             label-format="MMMM D, YYYY"
             modal-style="PickerModal"
+            modal-overlay-style="PickerModal__overlay"
             modal-text-style="PickerModal__text"
             name="df"
             placeholder="Select date"

examples/behaviors/blur.xml
@@ -83,6 +83,7 @@
         shadowOpacity="0.2"
         shadowRadius="5"
       />
+      <style id="PickerModal__overlay" backgroundColor="#1f1f1fa0" />
       <style
         id="PickerModal__text"
         color="blue"
@@ -200,6 +201,7 @@
             field-text-style="Input__Text"
             label-format="MMMM D, YYYY"
             modal-style="PickerModal"
+            modal-overlay-style="PickerModal__overlay"
             modal-text-style="PickerModal__text"
             name="date1"
             mode="spinner"

examples/behaviors/change.xml
@@ -83,6 +83,7 @@
         shadowOpacity="0.2"
         shadowRadius="5"
       />
+      <style id="PickerModal__overlay" backgroundColor="#1f1f1fa0" />
       <style
         id="PickerModal__text"
         color="blue"
@@ -252,6 +253,7 @@
             field-text-style="Input__Text"
             label-format="MMMM D, YYYY"
             modal-style="PickerModal"
+            modal-overlay-style="PickerModal__overlay"
             modal-text-style="PickerModal__text"
             name="date1"
             mode="spinner"

examples/behaviors/focus.xml
@@ -83,6 +83,7 @@
         shadowOpacity="0.2"
         shadowRadius="5"
       />
+      <style id="PickerModal__overlay" backgroundColor="#1f1f1fa0" />
       <style
         id="PickerModal__text"
         color="blue"
@@ -200,6 +201,7 @@
             field-text-style="Input__Text"
             label-format="MMMM D, YYYY"
             modal-style="PickerModal"
+            modal-overlay-style="PickerModal__overlay"
             modal-text-style="PickerModal__text"
             name="date1"
             mode="spinner"

examples/ui_elements/forms/date_field/index.xml.njk
@@ -41,6 +41,10 @@ tags: forms
     shadowOffsetY="-5"
     shadowOpacity="0.2"
     shadowRadius="5"/>
+  <style
+    id="PickerModal__overlay"
+    backgroundColor="#1f1f1fa0"
+  />
   <style id="help--error" color="#FF4847"/>
   <style
     id="Input"
@@ -66,6 +70,7 @@ tags: forms
     </modifier>
   </style>
   <style id="PickerModalCustom" backgroundColor="purple" borderTopColor="#E1E1E1" borderTopWidth="1"/>
+  <style id="PickerModalCustom_overlay" backgroundColor="purple" opacity="0.4" />
   <style id="PickerModalCustom__text" color="white" fontSize="20" fontWeight="600" padding="24" paddingBottom="0">
     <modifier pressed="true">
       <style color="yellow"/>
@@ -91,6 +96,7 @@ tags: forms
           field-style="Input"
           field-text-style="Input__Text"
           label-format="MMMM D, YYYY"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="date"
@@ -104,6 +110,7 @@ tags: forms
           field-text-style="Input__Text"
           label-format="MMMM D, YYYY"
           modal-style="PickerModal"
+          modal-overlay-style="PickerModal__overlay"
           modal-text-style="PickerModal__text"
           mode="spinner"
           name="date"
@@ -116,6 +123,7 @@ tags: forms
           field-style="Input"
           field-text-style="Input__Text"
           label-format="MMMM D, YYYY"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="date"
@@ -129,6 +137,7 @@ tags: forms
           field-style="Input"
           field-text-style="Input__Text"
           label-format="MMMM D, YYYY"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="date"
@@ -139,6 +148,7 @@ tags: forms
           field-style="Input"
           field-text-style="Input__Text"
           label-format="L"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="date"
@@ -149,6 +159,7 @@ tags: forms
           field-style="Input"
           field-text-style="Input__Text"
           label-format="MMMM [the] Do, [`]YY"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="date"
@@ -163,6 +174,7 @@ tags: forms
           field-text-style="Input__Text"
           label-format="MMMM D, YYYY"
           min="2018-05-05"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="date"
@@ -174,6 +186,7 @@ tags: forms
           label-format="MMMM D, YYYY"
           min="2018-05-05"
           modal-style="PickerModal"
+          modal-overlay-style="PickerModal__overlay"
           modal-text-style="PickerModal__text"
           mode="spinner"
           name="date"
@@ -187,6 +200,7 @@ tags: forms
           field-text-style="Input__Text"
           label-format="MMMM D, YYYY"
           max="2024-05-05"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="date"
@@ -198,6 +212,7 @@ tags: forms
           label-format="MMMM D, YYYY"
           max="2024-05-05"
           modal-style="PickerModal"
+          modal-overlay-style="PickerModal__overlay"
           modal-text-style="PickerModal__text"
           mode="spinner"
           name="date"
@@ -212,6 +227,7 @@ tags: forms
           label-format="MMMM D, YYYY"
           max="2024-05-05"
           min="2018-05-05"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="date"
@@ -224,6 +240,7 @@ tags: forms
           max="2024-05-05"
           min="2018-05-05"
           modal-style="PickerModal"
+          modal-overlay-style="PickerModal__overlay"
           modal-text-style="PickerModal__text"
           mode="spinner"
           name="date"
@@ -236,6 +253,7 @@ tags: forms
           field-style="Input Input--Error"
           field-text-style="Input__Text Input__Text--Error"
           label-format="MMMM D, YYYY"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="date"
@@ -249,6 +267,7 @@ tags: forms
           field-style="Input Input--Error"
           field-text-style="Input__Text Input__Text--Error"
           label-format="MMMM D, YYYY"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="date"
@@ -258,11 +277,30 @@ tags: forms
         <text style="help help--error">Please select a different date</text>
       </view>
       <view style="FormGroup">
+        <text style="label">Customized animation durations (iOS only)</text>
+        <date-field
+          field-style="Input"
+          field-text-style="Input__Text"
+          label-format="MMMM D, YYYY"
+          modal-overlay-style="PickerModal__overlay"
+          modal-style="PickerModal"
+          modal-text-style="PickerModal__text"
+          modal-animation-duration="400"
+          modal-overlay-animation-duration="400"
+          modal-dismiss-animation-duration="200"
+          modal-dismiss-overlay-animation-duration="200"
+          name="date"
+          placeholder="Select date"
+          placeholderTextColor="#8D9494"
+          value="2018-03-07"/>
+      </view>
+      <view style="FormGroup">
         <text style="label">Customized</text>
         <date-field
           field-style="InputCustom"
           field-text-style="InputCustom__Text"
           label-format="MMMM D, YYYY"
+          modal-overlay-style="PickerModalCustom_overlay"
           modal-style="PickerModalCustom"
           modal-text-style="PickerModalCustom__text"
           name="date"

examples/ui_elements/forms/picker/index.xml.njk
@@ -38,6 +38,10 @@ tags: forms
   <style id="help--error" color="#FF4847"/>
   <style id="Input__Text--Error" color="#FF4847"/>
   <style id="FormGroup" flex="1" marginLeft="24" marginRight="24" marginTop="48"/>
+  <style
+    id="PickerModal__overlay"
+    backgroundColor="#1f1f1fa0"
+  />
   <style id="PickerModal__text" color="blue" fontSize="16" fontWeight="600" marginBottom="16" padding="24">
     <modifier pressed="true">
       <style opacity="0.5"/>
@@ -87,6 +91,7 @@ tags: forms
         <picker-field
           field-style="Input"
           field-text-style="Input__Text"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="picker1"
@@ -106,6 +111,7 @@ tags: forms
         <picker-field
           field-style="Input"
           field-text-style="Input__Text"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="picker2"
@@ -126,6 +132,7 @@ tags: forms
         <picker-field
           field-style="Input"
           field-text-style="Input__Text"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="picker3"
@@ -147,6 +154,7 @@ tags: forms
         <picker-field
           field-style="Input Input--Error"
           field-text-style="Input__Text Input__Text--Error"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="picker4"
@@ -167,6 +175,7 @@ tags: forms
         <picker-field
           field-style="Input Input--Error"
           field-text-style="Input__Text Input__Text--Error"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModal"
           modal-text-style="PickerModal__text"
           name="picker5"
@@ -184,12 +193,38 @@ tags: forms
         <text style="help help--error">Please select a different choice</text>
       </view>
       <view style="FormGroup">
+        <text style="label">Customized animation durations (iOS only)</text>
+        <picker-field
+          field-style="Input"
+          field-text-style="Input__Text"
+          modal-overlay-style="PickerModal__overlay"
+          modal-style="PickerModal"
+          modal-text-style="PickerModal__text"
+          modal-animation-duration="400"
+          modal-overlay-animation-duration="400"
+          modal-dismiss-animation-duration="200"
+          modal-dismiss-overlay-animation-duration="200"
+          name="picker5"
+          placeholder="Select choice"
+          placeholderTextColor="#8D9494"
+          value="3">
+          <picker-item label="Choice 0" value="0"/>
+          <picker-item label="Choice 1" value="1"/>
+          <picker-item label="Choice 2" value="2"/>
+          <picker-item label="Choice 3" value="3"/>
+          <picker-item label="Choice 4" value="4"/>
+          <picker-item label="Choice 5" value="5"/>
+          <picker-item label="Choice 6" value="6"/>
+        </picker-field>
+      </view>
+      <view style="FormGroup">
         <text style="label">Customized</text>
         <picker-field
           cancel-label="Nevermind"
           done-label="Set choice"
           field-style="InputCustom"
           field-text-style="InputCustom__Text"
+          modal-overlay-style="PickerModal__overlay"
           modal-style="PickerModalCustom"
           modal-text-style="PickerModalCustom__text"
           name="picker6"

schema/core.xsd
@@ -969,6 +969,14 @@
       <xs:attribute name="field-style" type="hv:styleList" />
       <xs:attribute name="field-text-style" type="hv:styleList" />
       <xs:attribute name="modal-style" type="hv:styleList" />
+      <xs:attribute name="modal-overlay-style" type="hv:styleList" />
+      <xs:attribute name="modal-animation-duration" type="xs:integer" />
+      <xs:attribute name="modal-overlay-animation-duration" type="xs:integer" />
+      <xs:attribute name="modal-dismiss-animation-duration" type="xs:integer" />
+      <xs:attribute
+        name="modal-dismiss-overlay-animation-duration"
+        type="xs:integer"
+      />
       <xs:attribute name="modal-text-style" type="hv:styleList" />
       <xs:attribute name="id" type="hv:ID" />
       <xs:attribute name="key" type="hv:KEY" />
@@ -994,6 +1002,14 @@
       <xs:attribute name="field-style" type="hv:styleList" />
       <xs:attribute name="field-text-style" type="hv:styleList" />
       <xs:attribute name="modal-style" type="hv:styleList" />
+      <xs:attribute name="modal-overlay-style" type="hv:styleList" />
+      <xs:attribute name="modal-animation-duration" type="xs:integer" />
+      <xs:attribute name="modal-overlay-animation-duration" type="xs:integer" />
+      <xs:attribute name="modal-dismiss-animation-duration" type="xs:integer" />
+      <xs:attribute
+        name="modal-dismiss-overlay-animation-duration"
+        type="xs:integer"
+      />
       <xs:attribute name="modal-text-style" type="hv:styleList" />
       <xs:attribute name="id" type="hv:ID" />
       <xs:attribute name="key" type="hv:KEY" />

commit 475dc39cea703c182af58899139df13ad1caf188

fix(picker-field): Unwanted style (#670)
Remove `field-style` stylesheet that was applied to the intermediate
view wrapping the picker - this style is already applied to the actual
field UI (see src/components/hv-picker-field/field/index.js), and its
presence is causing improper rendering in Instawork mobile app.

This issue was introduced by recent refactor in #633 

| Before | After | Sanity check with Demo app |
|-------|--------|----------|
|
![before](https://github.com/Instawork/hyperview/assets/309515/a785ef6e-6bd0-4557-808d-7d1c641426fd)
|
![after](https://github.com/Instawork/hyperview/assets/309515/1918604a-5e37-4e5a-96a6-3a93865b3692)
|
![demo](https://github.com/Instawork/hyperview/assets/309515/20f977e1-b7dd-4c2f-bdea-f193fc76dcae)
|

Co-authored-by: flochtililoch <flochtililoch@gmail.com>

src/components/hv-picker-field/index.ios.js
@@ -176,15 +176,6 @@ export default class HvPickerField extends PureComponent<HvComponentProps> {
       style.push({ color: placeholderTextColor });
     }
 
-    const fieldStyle: Array<StyleSheet> = createStyleProp(
-      this.props.element,
-      this.props.stylesheets,
-      {
-        ...this.props.options,
-        styleAttr: 'field-style',
-      },
-    );
-
     // Gets all of the <picker-item> elements. All picker item elements
     // with a value and label are turned into options for the picker.
     const children = this.getPickerItems()
@@ -217,11 +208,7 @@ export default class HvPickerField extends PureComponent<HvComponentProps> {
             options={this.props.options}
             stylesheets={this.props.stylesheets}
           >
-            <View
-              accessibilityLabel={accessibilityLabel}
-              style={fieldStyle}
-              testID={testID}
-            >
+            <View accessibilityLabel={accessibilityLabel} testID={testID}>
               <Picker
                 onValueChange={this.setPickerValue}
                 selectedValue={this.getPickerValue()}