5562ca70dbc0cb2090f18cf07eb86261a7008980
Gorak <jamie@practicaluseful.com>
2023-09-02 18:28:10 +0800

    Updated Navidator.js to use SafeAreaProvider and SafeAreaView to resolve issue with Android header (Issue 550)

--- demo/src/Navigator.js
+++ demo/src/Navigator.js
@@ -11,27 +11,32 @@ import HyperviewScreen from './HyperviewScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator();
 
 export default () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Group>
-          <Stack.Screen
-            component={HyperviewScreen}
-            initialParams={{ url: Constants.ENTRY_POINT_URL }}
-            name={Constants.MAIN_STACK_NAME}
-          />
-        </Stack.Group>
-        <Stack.Group screenOptions={{ presentation: 'modal' }}>
-          <Stack.Screen
-            component={HyperviewScreen}
-            name={Constants.MODAL_STACK_NAME}
-          />
-        </Stack.Group>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaView style={{ flex: 1 }}>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Group>
+              <Stack.Screen
+                component={HyperviewScreen}
+                initialParams={{ url: Constants.ENTRY_POINT_URL }}
+                name={Constants.MAIN_STACK_NAME}
+              />
+            </Stack.Group>
+            <Stack.Group screenOptions={{ presentation: 'modal' }}>
+              <Stack.Screen
+                component={HyperviewScreen}
+                name={Constants.MODAL_STACK_NAME}
+              />
+            </Stack.Group>
+          </Stack.Navigator>
+        </SafeAreaView>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
