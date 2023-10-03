4aed9c93232f81fbbe2e2d66009925ab43ab6de6
Hardin Gray <hgray@instawork.com>
2023-10-02 23:36:33 -0400

    Strong typing story arguments

--- src/components/hv-option/stories/index.tsx
+++ src/components/hv-option/stories/index.tsx
@@ -9,31 +9,59 @@
 import * as Helpers from 'hyperview/storybook/helpers';
 import HvOption from 'hyperview/src/components/hv-option';
 import React from 'react';
+import type { StyleSheets } from 'hyperview/src/services/stylesheets';
 import { action } from '@storybook/addon-actions';
 
 const createStory = Helpers.stories(HvOption);
-createStory('basic', ({ element, stylesheets }) => (
-  <HvOption
-    element={element}
-    onUpdate={action('onUpdate')}
-    options={Helpers.getOptions()}
-    stylesheets={stylesheets}
-  />
-));
-createStory('pre_selected', ({ element, stylesheets }) => (
-  <HvOption
-    element={element}
-    onUpdate={action('onUpdate')}
-    options={Helpers.getOptions()}
-    stylesheets={stylesheets}
-  />
-));
+createStory(
+  'basic',
+  ({
+    element,
+    stylesheets,
+  }: {
+    element: Element;
+    stylesheets: StyleSheets;
+  }) => (
+    <HvOption
+      element={element}
+      onUpdate={action('onUpdate')}
+      options={Helpers.getOptions()}
+      stylesheets={stylesheets}
+    />
+  ),
+);
+createStory(
+  'pre_selected',
+  ({
+    element,
+    stylesheets,
+  }: {
+    element: Element;
+    stylesheets: StyleSheets;
+  }) => (
+    <HvOption
+      element={element}
+      onUpdate={action('onUpdate')}
+      options={Helpers.getOptions()}
+      stylesheets={stylesheets}
+    />
+  ),
+);
 
-createStory('custom', ({ element, stylesheets }) => (
-  <HvOption
-    element={element}
-    onUpdate={action('onUpdate')}
-    options={Helpers.getOptions()}
-    stylesheets={stylesheets}
-  />
-));
+createStory(
+  'custom',
+  ({
+    element,
+    stylesheets,
+  }: {
+    element: Element;
+    stylesheets: StyleSheets;
+  }) => (
+    <HvOption
+      element={element}
+      onUpdate={action('onUpdate')}
+      options={Helpers.getOptions()}
+      stylesheets={stylesheets}
+    />
+  ),
+);
