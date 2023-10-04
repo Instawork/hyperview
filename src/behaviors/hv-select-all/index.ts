68f340083d9d700f7789db7c3bea9b063dbfb9dd
flochtililoch <flochtililoch@gmail.com>
2023-10-04 13:50:53 -0700

    chore: migrate src/behaviors/hv-select-all

--- src/behaviors/hv-select-all/index.ts
+++ src/behaviors/hv-select-all/index.ts
@@ -1,11 +1,7 @@
-// @flow
-
 import * as Behaviors from 'hyperview/src/services/behaviors';
 import * as Xml from 'hyperview/src/services/xml';
 import type {
   DOMString,
-  Document,
-  Element,
   HvComponentOnUpdate,
   HvGetRoot,
   HvUpdateRoot,
@@ -21,7 +17,9 @@ export default {
     getRoot: HvGetRoot,
     updateRoot: HvUpdateRoot,
   ) => {
-    const targetId: ?DOMString = element.getAttribute('target');
+    const targetId: DOMString | null | undefined = element.getAttribute(
+      'target',
+    );
     if (!targetId) {
       console.warn('[behaviors/select-all]: missing "target" attribute');
       return;
@@ -40,7 +38,9 @@ export default {
 
     const selectAll = () => {
       const doc: Document = getRoot();
-      const targetElement: ?Element = doc.getElementById(targetId);
+      const targetElement: Element | null | undefined = doc.getElementById(
+        targetId,
+      );
       if (!targetElement) {
         return;
       }
