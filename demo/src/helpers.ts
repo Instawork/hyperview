21d1696aac290685a0492ff2ae9e4c3d45b34e54
Florent Bonomo <flochtililoch+github@gmail.com>
2023-10-09 10:39:06 -0700

    chore(ts): update demo app (#718)
    - chore: update dependencies (f989b735bd66ea7b6da06cafe872e1c126f4a7fa)
    - refactor: migrate code to TS
    (d595bf0d38b186f95bfc3e048b41478790be87c8)
    
    ---------
    
    Co-authored-by: flochtililoch <flochtililoch@gmail.com>

---
+++ demo/src/helpers.ts
@@ -0,0 +1,17 @@
+import moment from 'moment';
+
+export const formatDate = (date: Date | null | undefined, format: string | undefined) => moment(date).format(format);
+
+export const fetchWrapper = (input: RequestInfo | URL, init: RequestInit | undefined = { headers: {} }): Promise<Response> => {
+  return fetch(input, {
+    ...init,
+    mode: "cors",
+    headers: {
+      // Don't cache requests for the demo
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Expires: '0',
+      Pragma: 'no-cache',
+      ...init.headers,
+    }
+  });
+}
