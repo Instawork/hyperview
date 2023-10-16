21e47bb8d542cb342b42c2b047c6a17bbbee7d01
flochtililoch <flochtililoch@gmail.com>
2023-10-16 13:16:20 -0700

    fix: lint

--- src/components/hv-switch/style-sheet/index.ts
+++ src/components/hv-switch/style-sheet/index.ts
@@ -7,6 +7,7 @@
  */
 
 import type { ColorValue } from 'react-native';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import normalizeColor from 'react-native/Libraries/StyleSheet/normalizeColor';
 
