796af1c7bd60ed7183ef7f2f3be4d1cf479ec3e8
Hardin Gray <hgray@instawork.com>
2023-11-21 12:59:45 -0500

    chore: Adding copyrights where missing (#756)
    Found several places which were missing copyrights. I think I got them
    all.
    
    
    Asana: https://app.asana.com/0/1204008699308084/1205996503347223/f

--- demo/src/helpers.ts
+++ demo/src/helpers.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Garuda Labs, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import moment from 'moment';
 
 export const formatDate = (date: Date | null | undefined, format: string | undefined) => moment(date).format(format);
