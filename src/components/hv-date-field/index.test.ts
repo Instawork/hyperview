29fc2343684b97114a164a25b50208e2fb4234eb
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-11-27 18:32:40 +0100

    fix: format single digit months in date-field (#758)
    Fixes #757 
    
    Previously, dates where the month is a single digit (January – September
    / 1 - 9) created formatted dates like `2023-1-27`. This would break
    parsing the date as an ISO string in certain languages.
    
    This PR pads the month part with a leading zero to make it correctly
    formatted as `YYYY-MM-DD`.

--- src/components/hv-date-field/index.test.ts
+++ src/components/hv-date-field/index.test.ts
@@ -43,4 +43,40 @@ describe('HvDateField', () => {
       expect(HvDateField.getFormInputValues(elements[2])).toEqual([]);
     });
   });
+
+  describe('createStringFromDate', () => {
+    it('returns empty string if no date', () => {
+      expect(HvDateField.createStringFromDate(null)).toEqual('');
+    });
+
+    it('returns correct string for double digit month', () => {
+      expect(HvDateField.createStringFromDate(new Date(2023, 10, 12))).toEqual(
+        '2023-11-12',
+      );
+    });
+
+    it('returns correct string for single digit month', () => {
+      expect(HvDateField.createStringFromDate(new Date(2023, 6, 12))).toEqual(
+        '2023-07-12',
+      );
+    });
+  });
+
+  describe('createDateFromString', () => {
+    it('returns null if no date', () => {
+      expect(HvDateField.createDateFromString(null)).toBeNull();
+    });
+
+    it('returns correct date for double digit month', () => {
+      expect(HvDateField.createDateFromString('2023-11-12')).toEqual(
+        new Date(2023, 10, 12),
+      );
+    });
+
+    it('returns correct date for single digit month', () => {
+      expect(HvDateField.createDateFromString('2023-07-12')).toEqual(
+        new Date(2023, 6, 12),
+      );
+    });
+  });
 });
