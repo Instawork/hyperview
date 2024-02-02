1acbf047b2fb64c307fa472f26559927fedb1690
Hardin Gray <hgray@instawork.com>
2024-02-01 20:22:30 -0800

    Set up demo to toggle between external and internal navigator with a base HXML navigator structure

--- demo/src/Navigator.tsx
+++ demo/src/Navigator.tsx
@@ -17,48 +17,98 @@ import React from 'react';
 import type { RootStackParamList } from './types';
 import { View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Hyperview from 'hyperview';
+import { fetchWrapper, formatDate } from './helpers';
+import { ToggleSwitch } from './ToggleSwitch';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
-const Navigation = () => {
+/**
+ * Use a toggle to switch between internal and external navigators.
+ */
+const ToggleView = () => {
+  const [useInternalNavigator, setUseInternalNavigator] = React.useState(false);
+  const toggleValue = () =>
+    setUseInternalNavigator(previousState => !previousState);
   const insets = useSafeAreaInsets();
+
   return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <View
+        style={{
+          paddingLeft: insets.left + 24,
+          paddingRight: insets.right + 24,
+          paddingTop: insets.top,
+          paddingBottom: 12,
+        }}
+      >
+        <ToggleSwitch
+          isEnabled={useInternalNavigator}
+          toggleValue={toggleValue}
+        />
+      </View>
       <View
         style={{
           flex: 1,
 
-          // Paddings to handle safe area
+          // Padding to handle safe area
           paddingBottom: insets.bottom,
           paddingLeft: insets.left,
           paddingRight: insets.right,
-          paddingTop: insets.top,
         }}
       >
         <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Group>
-              <Stack.Screen
-                component={HyperviewScreen}
-                initialParams={{ url: Constants.ENTRY_POINT_URL }}
-                name={Constants.MAIN_STACK_NAME}
-              />
-            </Stack.Group>
-            <Stack.Group screenOptions={{ presentation: 'modal' }}>
-              <Stack.Screen
-                component={HyperviewScreen}
-                name={Constants.MODAL_STACK_NAME}
-              />
-            </Stack.Group>
-          </Stack.Navigator>
+          {useInternalNavigator ? <InternalNavigator /> : <ExternalNavigator />}
         </NavigationContainer>
       </View>
+    </View>
+  );
+};
+
+/**
+ * Provide an external navigator to Hyperview
+ */
+const ExternalNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Group>
+        <Stack.Screen
+          component={HyperviewScreen}
+          initialParams={{ url: Constants.ENTRY_POINT_URL }}
+          name={Constants.MAIN_STACK_NAME}
+        />
+      </Stack.Group>
+      <Stack.Group screenOptions={{ presentation: 'modal' }}>
+        <Stack.Screen
+          component={HyperviewScreen}
+          name={Constants.MODAL_STACK_NAME}
+        />
+      </Stack.Group>
+    </Stack.Navigator>
+  );
+};
+
+/**
+ * Hyperview will create an internal Navigator from the document HXML
+ */
+const InternalNavigator = () => {
+  return (
+    <Hyperview
+      entrypointUrl={Constants.ENTRY_POINT_NAV_URL}
+      fetch={fetchWrapper}
+      formatDate={formatDate}
+    />
   );
 };
 
 export default () => {
   return (
     <SafeAreaProvider>
-      <Navigation />
+      <ToggleView />
     </SafeAreaProvider>
   );
 };
--- demo/src/constants.ts
+++ demo/src/constants.ts
@@ -9,6 +9,9 @@
 // This is the URL of the service running the Hyperview examples
 export const ENTRY_POINT_URL = 'http://0.0.0.0:8085/index.xml';
 
+// This is the URL of the service running a navigator example
+export const ENTRY_POINT_NAV_URL = 'http://0.0.0.0:8085/index_navigator.xml';
+
 export const MAIN_STACK_NAME = 'Main';
 
 export const MODAL_STACK_NAME = 'Modal';
