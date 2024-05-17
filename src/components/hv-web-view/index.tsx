89b077a3e88ef118a5236911863e9c41f4d13204
Sourabh Punja <sdinkarapunja@instawork.com>
2024-05-16 23:28:32 -0400

    remove allows

--- src/components/hv-web-view/index.tsx
+++ src/components/hv-web-view/index.tsx
@@ -37,7 +37,9 @@ export default class HvWebView extends PureComponent<HvComponentProps> {
       this.props.stylesheets,
       this.props.options,
     );
-    const allowsInlineMediaPlayback = props['allows-inline-media-playback'] ? props['allows-inline-media-playback'] === 'true'  : undefined;
+    const allowsInlineMediaPlayback = props["allows-inline-media-playback"]
+      ? props["allows-inline-media-playback"] === "true"
+      : undefined;
     const color = props['activity-indicator-color'] || '#8d9494';
     const injectedJavaScript = props['injected-java-script'];
     const source = { html: props.html, uri: props.url } as const;
