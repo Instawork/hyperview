8016383a4be15342af3598eb2792208cba902ee0
rohankumar1999 <rbayya@instawork.com>
2024-06-03 17:23:35 -0700

    adding custom handler for stopLoader event

--- src/components/hv-web-view/index.tsx
+++ src/components/hv-web-view/index.tsx
@@ -1,6 +1,6 @@
 import * as Events from 'hyperview/src/services/events';
 import * as Namespaces from 'hyperview/src/services/namespaces';
-import { ActivityIndicator, StyleSheet } from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import React, { PureComponent } from 'react';
 import type { HvComponentProps } from 'hyperview/src/types';
 import { LOCAL_NAME } from 'hyperview/src/types';
@@ -14,6 +14,10 @@ export default class HvWebView extends PureComponent<HvComponentProps> {
 
   static localNameAliases = [];
 
+  state = {
+    loading: true,
+  };
+
   onMessage = (
     event: {
       nativeEvent: {
@@ -25,10 +29,21 @@ export default class HvWebView extends PureComponent<HvComponentProps> {
       return;
     }
     const matches = event.nativeEvent.data.match(/^hyperview:(.*)$/);
+    const stopLoaderEvent = event.nativeEvent.data.match(/^stopLoader$/);
     if (matches) {
       Events.dispatch(matches[1]);
+    } else if (stopLoaderEvent) {
+      this.setState({loading: false});
     }
   };
+
+  renderLoading = (color:string) => {
+    return this.state.loading ? 
+    <ActivityIndicator
+      color={color}
+      style={StyleSheet.absoluteFillObject}
+    /> : <View/>
+  }
 
   render() {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -41,19 +56,15 @@ export default class HvWebView extends PureComponent<HvComponentProps> {
       ? props['allows-inline-media-playback'] === 'true'
       : undefined;
     const color = props['activity-indicator-color'] || '#8d9494';
-    const injectedJavaScript = props['injected-java-script'];
+    const domContentBehavior = props['dom-content-behavior'] || "";
+    const injectedJavaScript = props['injected-java-script'] + domContentBehavior;
     const source = { html: props.html, uri: props.url } as const;
     return (
       <WebView
         allowsInlineMediaPlayback={allowsInlineMediaPlayback}
         injectedJavaScript={injectedJavaScript}
         onMessage={this.onMessage}
-        renderLoading={() => (
-          <ActivityIndicator
-            color={color}
-            style={StyleSheet.absoluteFillObject}
-          />
-        )}
+        renderLoading={() => this.renderLoading(color)}
         source={source}
         startInLoadingState
       />
