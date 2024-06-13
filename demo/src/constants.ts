1624493af64312bcb051db2a13d228332b796e92
Adam Stepinski <adam@instawork.com>
2024-06-13 11:24:14 -0700

    cleanup

--- demo/src/constants.ts
+++ demo/src/constants.ts
@@ -1,5 +1,5 @@
 // This is the URL of the service running the Hyperview examples
-export const ENTRY_POINT_URL = 'http://0.0.0.0:8085/search.xml';
+export const ENTRY_POINT_URL = 'http://0.0.0.0:8085/index.xml';
 
 // This is the URL of the service running a navigator example
 export const ENTRY_POINT_NAV_URL = 'http://0.0.0.0:8085/index_navigator.xml';
