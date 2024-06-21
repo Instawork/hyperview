71cd3662a8238dbe78355064b6652829cbc5b3eb
Hardin Gray <hgray@instawork.com>
2024-06-20 20:46:10 -0400

    Simplified implementation

--- src/services/logging/default-logger.ts
+++ src/services/logging/default-logger.ts
@@ -3,32 +3,27 @@
  * The default logger performs toString() operations on each param passed to the logging actions
  */
 export class DefaultLogger {
-  // Provides a forced toString conversion of any params which support a toString() function
-  asString = (o: any): any => {
-    return typeof o.toString === 'function' ? String(o) : o;
-  };
-
   log = (m?: any, ...p: any[]): void =>
     console.log(
-      this.asString(m),
-      p.map(param => this.asString(param)),
+      String(m),
+      p.map(param => String(param)),
     );
 
   info = (m?: any, ...p: any[]): void =>
     console.info(
-      this.asString(m),
-      p.map(param => this.asString(param)),
+      String(m),
+      p.map(param => String(param)),
     );
 
   warn = (m?: any, ...p: any[]): void =>
     console.warn(
-      this.asString(m),
-      p.map(param => this.asString(param)),
+      String(m),
+      p.map(param => String(param)),
     );
 
   error = (m?: any, ...p: any[]): void =>
     console.error(
-      this.asString(m),
-      p.map(param => this.asString(param)),
+      String(m),
+      p.map(param => String(param)),
     );
 }
