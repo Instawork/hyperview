8c25c4368eb424ba1e035971f6eff386047d6391
Hardin Gray <hgray@instawork.com>
2024-06-21 13:42:07 -0400

    Only force string on instances of `ToStringHelper` to avoid string conversion of other objects

--- src/services/logging/default-logger.ts
+++ src/services/logging/default-logger.ts
@@ -1,29 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { ToStringHelper } from './tostring-helper';
+
 /**
  * The default logger performs toString() operations on each param passed to the logging actions
  */
 export class DefaultLogger {
+  toString = (m?: any): any => {
+    if (m instanceof ToStringHelper) {
+      return m.toString();
+    }
+    return m;
+  };
+
   log = (m?: any, ...p: any[]): void =>
     console.log(
-      String(m),
-      p.map(param => String(param)),
+      this.toString(m),
+      p.map(param => this.toString(param)),
     );
 
   info = (m?: any, ...p: any[]): void =>
     console.info(
-      String(m),
-      p.map(param => String(param)),
+      this.toString(m),
+      p.map(param => this.toString(param)),
     );
 
   warn = (m?: any, ...p: any[]): void =>
     console.warn(
-      String(m),
-      p.map(param => String(param)),
+      this.toString(m),
+      p.map(param => this.toString(param)),
     );
 
   error = (m?: any, ...p: any[]): void =>
     console.error(
-      String(m),
-      p.map(param => String(param)),
+      this.toString(m),
+      p.map(param => this.toString(param)),
     );
 }
