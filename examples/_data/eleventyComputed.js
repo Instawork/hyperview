900be04d13d6961dee2fc84351bfa3fa31a65415
rohanbayya <rbayya@instawork.com>
2024-08-08 13:54:45 -0700

    add permalink automatically

--- examples/_data/eleventyComputed.js
+++ examples/_data/eleventyComputed.js
@@ -1,18 +1,11 @@
 module.exports = {
-  permalink: function (data) {
+  permalink(data) {
     // Check if the file extension is .xml.njk
     if (data.page.inputPath.endsWith('.xml.njk')) {
       // Generate permalink by replacing .xml.njk with .xml
-      console.log(
-        'eleventy : ',
-        data.page.inputPath,
-        '  ',
-        data.page.filePathStem,
-      );
       return data.page.filePathStem;
     }
     // Default behavior for other files
-    // console.log('eleventy : ', data.permalink);
     return data.permalink;
   },
 };
