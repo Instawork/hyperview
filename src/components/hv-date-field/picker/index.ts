0061e06c36fe9806a03cdd897ac1f381741f1741
flochtililoch <flochtililoch@gmail.com>
2024-09-24 13:15:26 -0700

    feat(hv-date-field): Implement web version Build on previous refactor to add support for native date picker on web.
    Solves #454

--- src/components/hv-date-field/picker/index.ts
+++
@@ -1,4 +0,0 @@
-import type { Props } from './types';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
-export default (p: Props) => null;
--- src/core/components/date-time-picker/index.web.ts
+++
@@ -1,6 +0,0 @@
-// Currently not implemented, should be addressed in
-// https://github.com/Instawork/hyperview/issues/454
-export default () => {
-  alert('Not implemented!'); // eslint-disable-line no-alert
-  return '';
-};
