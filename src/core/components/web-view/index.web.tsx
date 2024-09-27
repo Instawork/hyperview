0ea554f77710195101759ef4214d2e64c4e1bb10
flochtililoch <flochtililoch@gmail.com>
2024-09-27 11:20:53 -0700

    fix(webview): rendering on web Render the actual HTML content as a node instead of returning it as a string

--- src/core/components/web-view/index.web.tsx
+++ src/core/components/web-view/index.web.tsx
@@ -1,25 +1,33 @@
 import { Dimensions } from 'react-native';
 import React from 'react';
+import type { WebViewProps } from 'react-native-webview';
 
 // Currently partially implemented, should be brought to parity
 // with mobile in https://github.com/Instawork/hyperview/issues/455
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default (props: any) => {
+export default (props: WebViewProps) => {
   const windowDimensions = Dimensions.get('window');
   const windowHeight = windowDimensions.height;
   const windowWidth = windowDimensions.width;
-  if (props.source.uri) {
+  const { source } = props;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore Property 'uri' does not exist on type 'WebViewSource'.ts(2339)
+  if (source?.uri) {
     return (
       <iframe
         height={windowHeight}
-        src={props.source.uri}
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore Property 'uri' does not exist on type 'WebViewSource'.ts(2339)
+        src={props.source?.uri}
         title="-"
         width={windowWidth}
       />
     );
   }
-  if (props.source.html) {
-    return props.source.html;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore Property 'html' does not exist on type 'WebViewSourceHtml'.ts(2339)
+  if (props.source?.html) {
+    // eslint-disable-next-line react/no-danger
+    return <div dangerouslySetInnerHTML={{ __html: props.source.html }} />;
   }
   return '';
 };
