74e4a7ef076616900691410b5fa84f2766875a1a
flochtililoch <flochtililoch@gmail.com>
2024-09-30 20:25:28 -0700

    chore: collocate example files with demo app Since we are already creating the cache directory under the demo directory, and that both XML server and demo app are tightly coupled.

--- examples/rename_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-find . -type f -name "_*" | while read file; do
-    dir=$(dirname "$file")
-    base=$(basename "$file")
-    new_base="${base#_}"
-    mv "$file" "$dir/$new_base"
-done
-
