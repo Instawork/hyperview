49dce50a966b8e14e9d702446a932780ef096522
Ankit Desai <adesai@instawork.com>
2024-11-22 09:57:08 -0500

    new BottomSheet WIP

---
+++ demo/src/Components/BottomSheet/BottomSheetStopPoint.tsx
@@ -0,0 +1,22 @@
+import type { HvComponentProps, LocalName } from 'hyperview';
+import Hyperview from 'hyperview';
+import React, { useState } from 'react';
+import { LayoutChangeEvent, View } from 'react-native';
+
+const namespace = 'https://hyperview.org/bottom-sheet';
+
+const BottomSheetStopPoint = (props: HvComponentProps) => {
+  const children = Hyperview.renderChildren(
+    props.element,
+    props.stylesheets,
+    props.onUpdate,
+    props.options,
+  );
+  return <View>{children}</View>;
+};
+
+BottomSheetStopPoint.namespaceURI = namespace;
+BottomSheetStopPoint.localName = 'stop-point' as LocalName;
+BottomSheetStopPoint.localNameAliases = [] as LocalName[];
+
+export { BottomSheetStopPoint };
