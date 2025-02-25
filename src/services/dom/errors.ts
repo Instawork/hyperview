67183b6d022de14975ac4e9386cbe53acb3b081d
Hardin Gray <hgray@instawork.com>
2025-02-25 15:37:31 -0500

    chore(errors): improve quality of dom errors (#1095)
    The existing dom/errors were not extending `HvBaseError` so did [not log
    with context and improved
    fingerprinting](https://github.com/Instawork/mobile/blob/75f452fa06e72da720d57c4446ccfffcb2c4caae/core-mobile/src/services/error-reports/index.ts#L51-L57).
    
    Before:
    https://instawork.sentry.io/issues/6233499416/?project=1549809&project=1549805&query=for%20tag%20%3Ca%3E&referrer=issue-stream&sort=date&statsPeriod=14d&stream_index=0
    
    After:
    https://instawork.sentry.io/issues/6332099191/?project=1549805&query=&referrer=issue-stream&sort=date&statsPeriod=1h&stream_index=0#exception
    
    [Asana](https://app.asana.com/0/1204008699308084/1209319270405176)

--- src/services/dom/errors.ts
+++ src/services/dom/errors.ts
@@ -4,48 +4,52 @@ import * as ErrorService from 'hyperview/src/services/error';
 import type { LocalName } from 'hyperview/src/types';
 import { XMLSerializer } from '@instawork/xmldom';
 
-export class UnsupportedContentTypeError extends Error {
+export class UnsupportedContentTypeError extends ErrorService.HvBaseError {
   name = 'UnsupportedContentTypeError';
 }
 
 /**
  * XML parser errors
  */
-export class XMLParserWarning extends Error {
+export class XMLParserWarning extends ErrorService.HvBaseError {
   name = 'ParserWarning';
 }
 
-export class XMLParserError extends Error {
+export class XMLParserError extends ErrorService.HvBaseError {
   name = 'ParserError';
 }
 
-export class XMLParserFatalError extends Error {
+export class XMLParserFatalError extends ErrorService.HvBaseError {
   name = 'ParserFatalError';
 }
 
 /**
  * XML validation errors
  */
-export class XMLRequiredElementNotFound extends Error {
+export class XMLRequiredElementNotFound extends ErrorService.HvBaseError {
   name = 'XMLRequiredElementNotFound';
 
   constructor(tag: LocalName, url: string) {
     super(`Required <${tag}> tag not found in the response from ${url}`);
+    this.setExtraContext('tag', tag);
+    this.setExtraContext('url', url);
   }
 }
 
-export class XMLRestrictedElementFound extends Error {
+export class XMLRestrictedElementFound extends ErrorService.HvBaseError {
   name = 'XMLRestrictedElementFound';
 
   constructor(tag: LocalName, url: string) {
     super(`Restricted <${tag}> tag found in the response from ${url}`);
+    this.setExtraContext('tag', tag);
+    this.setExtraContext('url', url);
   }
 }
 
 /**
  * Fetch errors
  */
-export class ServerError extends Error {
+export class ServerError extends ErrorService.HvBaseError {
   name = 'ServerError';
 
   responseHeaders: Headers;
@@ -64,6 +68,10 @@ export class ServerError extends Error {
     this.responseText = responseText;
     this.responseHeaders = responseHeaders;
     this.status = status;
+    this.setExtraContext('url', url);
+    this.setExtraContext('responseText', responseText);
+    this.setExtraContext('responseHeaders', responseHeaders);
+    this.setExtraContext('status', status);
   }
 }
 
