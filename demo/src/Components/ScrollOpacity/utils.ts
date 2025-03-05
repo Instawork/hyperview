6730e9a45397fa9b8efc174706b1e712d4561af2
Florent Bonomo <flochtililoch+github@gmail.com>
2025-03-05 17:28:09 -0500

    refactor(demo/scroll-opacity): use ranges to map position with opacity (#1112)
    To increase flexibiility of the component, read position (input) and
    opacity (output) ranges from XML attribute, and use these to calculate
    the expected opacity. Update the example to show-case two different use.
    Also add the ability to pass styles to the wrapping view.
    
    
    
    https://github.com/user-attachments/assets/11c12a96-55a9-46f9-bc2e-1e0968298d69
    
    ---------
    
    Co-authored-by: flochtililoch <flochtililoch@gmail.com>

--- demo/src/Components/ScrollOpacity/utils.ts
+++ demo/src/Components/ScrollOpacity/utils.ts
@@ -3,11 +3,55 @@ export const namespaceURI = 'https://hyperview.org/scroll-opacity';
 export const getNumberAttr = (
   element: Element,
   attrName: string,
-  defaultValue: number | null = null,
+  defaultValue: number,
 ): number => {
   const value: string | null = element.getAttributeNS(namespaceURI, attrName);
   if (!value) {
-    return defaultValue || 0;
+    return defaultValue;
   }
   return parseInt(value, 10);
+};
+
+export const getRangeAttr = (
+  element: Element,
+  attrName: string,
+  defaultValue: [number, number],
+): [number, number] => {
+  const value: string | null = element.getAttributeNS(namespaceURI, attrName);
+  if (!value) {
+    return defaultValue;
+  }
+  try {
+    return JSON.parse(value);
+  } catch (e) {
+    throw new Error(`Invalid range attribute: ${value}`);
+  }
+};
+
+export const calculateOpacity = (
+  position: number,
+  inputRange: [number, number],
+  outputRange: [number, number],
+) => {
+  const [a, b] = inputRange;
+  const [c, d] = outputRange;
+
+  // Check if the input range has the same start and end values
+  if (a === b) {
+    // If the position is exactly at a (or b), return the start of the output range
+    // Otherwise, return the end of the output range
+    return position <= a ? c : d;
+  }
+
+  // Calculate the slope
+  const slope = (d - c) / (b - a);
+
+  // Calculate the opacity based on the position
+  if (position < a) {
+    return c;
+  }
+  if (position > b) {
+    return d;
+  }
+  return c + slope * (position - a);
 };
