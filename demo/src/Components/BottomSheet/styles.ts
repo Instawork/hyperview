e51754577f06faae6105803f5312d623d09832a0
Ankit Desai <331228+kitdesai@users.noreply.github.com>
2025-03-13 13:37:22 -0400

    update bottom sheet designs (#1120)
    <img
    src="https://github.com/user-attachments/assets/6ddede53-93a0-4164-bbbb-985ef2ceadfd"
    width="250" />
    
    <img
    src="https://github.com/user-attachments/assets/17b92b80-d124-4f98-b297-67a2866adbc7"
    width="250" />

--- demo/src/Components/BottomSheet/styles.ts
+++ demo/src/Components/BottomSheet/styles.ts
@@ -1,26 +1,17 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  bottomSheetContainer: {
-    backgroundColor: '#fff',
+  container: {
     position: 'absolute',
     width: '100%',
-  },
-  container: {
-    left: 0,
-    position: 'absolute',
-    right: 0,
   },
   handle: {
     alignSelf: 'center',
     backgroundColor: 'gray',
     borderRadius: 25,
     height: 4,
-    marginVertical: 15,
+    marginTop: 15,
     width: 75,
-  },
-  overflow: {
-    overflow: 'hidden',
   },
   overlay: {
     bottom: 0,
