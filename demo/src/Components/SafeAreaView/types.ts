df387fe49397d3c00fc31eb0ee86633a6df0cd4c
flochtililoch <flochtililoch@gmail.com>
2025-03-28 09:26:56 -0400

    feat(demo): add new safe-area custom element This element creates a view container and applies safe area insets as padding or margin attributes based on chosen mode.
    Note: implementation uses `useSafeAreaInsets` instead of `SafeAreaView` as a recommended [here](https://reactnavigation.org/docs/6.x/handling-safe-area/). This leads to a slighly more complex implementation but prevents a "jumpy" behavior on initial render.

---
+++ demo/src/Components/SafeAreaView/types.ts
@@ -0,0 +1,10 @@
+export type InsetsStyle = {
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
+  marginTop?: number;
+  paddingBottom?: number;
+  paddingLeft?: number;
+  paddingRight?: number;
+  paddingTop?: number;
+};
