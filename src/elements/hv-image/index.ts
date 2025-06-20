f5c352cfd23c9bc747918ed39721d794fecb22e3
Hardin Gray <hgray@instawork.com>
2025-06-20 09:50:55 -0400

    chore: convert to FC

--- src/elements/hv-image/index.ts
+++ src/elements/hv-image/index.ts
@@ -4,43 +4,38 @@ import type {
   HvComponentProps,
 } from 'hyperview/src/types';
 import { Image, ImageProps } from 'react-native';
-import React, { PureComponent } from 'react';
 import { LOCAL_NAME } from 'hyperview/src/types';
+import React from 'react';
 import { addHref } from 'hyperview/src/core/components/hyper-ref';
 import { createProps } from 'hyperview/src/services';
 import urlParse from 'url-parse';
 
-export default class HvImage extends PureComponent<HvComponentProps> {
-  static namespaceURI = Namespaces.HYPERVIEW;
-
-  static localName = LOCAL_NAME.IMAGE;
-
-  render() {
-    const { skipHref } = this.props.options || {};
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const imageProps: Record<string, any> = {};
-    let source = this.props.element.getAttribute('source');
-    if (source) {
-      source = urlParse(source, this.props.options.screenUrl, true).toString();
-      imageProps.source = { uri: source };
-    }
-    const props = {
-      ...createProps(
-        this.props.element,
-        this.props.stylesheets,
-        this.props.options,
-      ),
-      ...imageProps,
-    } as ImageProps;
-    const component = React.createElement(Image, props);
-    return skipHref
-      ? component
-      : addHref(
-          component,
-          this.props.element,
-          this.props.stylesheets,
-          this.props.onUpdate as HvComponentOnUpdate,
-          this.props.options,
-        );
+const HvImage = (props: HvComponentProps) => {
+  const { skipHref } = props.options || {};
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const imageProps: Record<string, any> = {};
+  let source = props.element.getAttribute('source');
+  if (source) {
+    source = urlParse(source, props.options.screenUrl, true).toString();
+    imageProps.source = { uri: source };
   }
-}
+  const componentProps = {
+    ...createProps(props.element, props.stylesheets, props.options),
+    ...imageProps,
+  } as ImageProps;
+  const component = React.createElement(Image, componentProps);
+  return skipHref
+    ? component
+    : addHref(
+        component,
+        props.element,
+        props.stylesheets,
+        props.onUpdate as HvComponentOnUpdate,
+        props.options,
+      );
+};
+
+HvImage.namespaceURI = Namespaces.HYPERVIEW;
+HvImage.localName = LOCAL_NAME.IMAGE;
+
+export default HvImage;
