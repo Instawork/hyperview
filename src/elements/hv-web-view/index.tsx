fa895b6f3b1aa9c81673fbc1607a83afef080db7
Hardin Gray <hgray@instawork.com>
2025-06-20 16:03:57 -0400

    chore: convert to FC

--- src/elements/hv-web-view/index.tsx
+++ src/elements/hv-web-view/index.tsx
@@ -1,22 +1,15 @@
 import * as Events from 'hyperview/src/services/events';
 import * as Namespaces from 'hyperview/src/services/namespaces';
 import { ActivityIndicator, StyleSheet } from 'react-native';
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import type { HvComponentProps } from 'hyperview/src/types';
 import { LOCAL_NAME } from 'hyperview/src/types';
 import WebView from 'hyperview/src/core/components/web-view';
 import { createProps } from 'hyperview/src/services';
 
-export default class HvWebView extends PureComponent<HvComponentProps> {
-  static namespaceURI = Namespaces.HYPERVIEW;
-
-  static localName = LOCAL_NAME.WEB_VIEW;
-
-  state = {
-    renderLoading: true,
-  };
-
-  onMessage = (
+const HvWebView = (props: HvComponentProps) => {
+  const [renderLoading, setRenderLoading] = useState(true);
+  const onMessage = (
     event: {
       nativeEvent: {
         data: string;
@@ -28,7 +21,7 @@ export default class HvWebView extends PureComponent<HvComponentProps> {
     }
 
     if (event.nativeEvent.data === 'hv-web-view:render-loading:false') {
-      this.setState({ renderLoading: false });
+      setRenderLoading(false);
     }
     const matches = event.nativeEvent.data.match(/^hyperview:(.*)$/);
     if (matches) {
@@ -36,48 +29,56 @@ export default class HvWebView extends PureComponent<HvComponentProps> {
     }
   };
 
-  render() {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const props: any = createProps(
-      this.props.element,
-      this.props.stylesheets,
-      this.props.options,
-    );
-    const allowsInlineMediaPlayback = props['allows-inline-media-playback']
-      ? props['allows-inline-media-playback'] === 'true'
-      : undefined;
-    const color = props['activity-indicator-color'] || '#8d9494';
-    const loadBehavior = props['show-loading-indicator'];
-    let injectedJavaScript = props['injected-java-script'];
-    if (loadBehavior === 'document-only') {
-      injectedJavaScript +=
-        'window.ReactNativeWebView.postMessage("hv-web-view:render-loading:false");';
-    }
-    const sharedCookiesEnabled = props['shared-cookies-enabled']
-      ? props['shared-cookies-enabled'] === 'true'
-      : undefined;
-    const source = { html: props.html, uri: props.url } as const;
-    return (
-      <WebView
-        accessibilityLabel={props.accessibilityLabel}
-        allowsInlineMediaPlayback={allowsInlineMediaPlayback}
-        injectedJavaScript={injectedJavaScript}
-        onMessage={this.onMessage}
-        renderLoading={() => {
-          return this.state.renderLoading ? (
-            <ActivityIndicator
-              color={color}
-              style={StyleSheet.absoluteFillObject}
-            />
-          ) : (
-            <></>
-          );
-        }}
-        sharedCookiesEnabled={sharedCookiesEnabled}
-        source={source}
-        startInLoadingState
-        testID={props.testID}
-      />
-    );
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const componentProps: any = createProps(
+    props.element,
+    props.stylesheets,
+    props.options,
+  );
+  const allowsInlineMediaPlayback = componentProps[
+    'allows-inline-media-playback'
+  ]
+    ? componentProps['allows-inline-media-playback'] === 'true'
+    : undefined;
+  const color = componentProps['activity-indicator-color'] || '#8d9494';
+  const loadBehavior = componentProps['show-loading-indicator'];
+  let injectedJavaScript = componentProps['injected-java-script'];
+  if (loadBehavior === 'document-only') {
+    injectedJavaScript +=
+      'window.ReactNativeWebView.postMessage("hv-web-view:render-loading:false");';
   }
-}
+  const sharedCookiesEnabled = componentProps['shared-cookies-enabled']
+    ? componentProps['shared-cookies-enabled'] === 'true'
+    : undefined;
+  const source = {
+    html: componentProps.html,
+    uri: componentProps.url,
+  } as const;
+  return (
+    <WebView
+      accessibilityLabel={componentProps.accessibilityLabel}
+      allowsInlineMediaPlayback={allowsInlineMediaPlayback}
+      injectedJavaScript={injectedJavaScript}
+      onMessage={onMessage}
+      renderLoading={() => {
+        return renderLoading ? (
+          <ActivityIndicator
+            color={color}
+            style={StyleSheet.absoluteFillObject}
+          />
+        ) : (
+          <></>
+        );
+      }}
+      sharedCookiesEnabled={sharedCookiesEnabled}
+      source={source}
+      startInLoadingState
+      testID={componentProps.testID}
+    />
+  );
+};
+
+HvWebView.namespaceURI = Namespaces.HYPERVIEW;
+HvWebView.localName = LOCAL_NAME.WEB_VIEW;
+
+export default HvWebView;
