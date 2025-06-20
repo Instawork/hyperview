582dc6cb14937880e6c3215060592be6376515f9
Hardin Gray <hgray@instawork.com>
2025-06-20 12:51:23 -0400

    chore: convert to FC

--- src/elements/hv-spinner/index.tsx
+++ src/elements/hv-spinner/index.tsx
@@ -1,24 +1,24 @@
 import * as Namespaces from 'hyperview/src/services/namespaces';
-import React, { PureComponent } from 'react';
 import { ActivityIndicator } from 'react-native';
 import type { HvComponentProps } from 'hyperview/src/types';
 import { LOCAL_NAME } from 'hyperview/src/types';
+import React from 'react';
 import { createTestProps } from 'hyperview/src/services';
 
-export default class HvSpinner extends PureComponent<HvComponentProps> {
-  static namespaceURI = Namespaces.HYPERVIEW;
+const HvSpinner = (props: HvComponentProps) => {
+  const color = props.element.getAttribute('color') || '#8d9494';
+  const { testID, accessibilityLabel } = createTestProps(props.element);
 
-  static localName = LOCAL_NAME.SPINNER;
+  return (
+    <ActivityIndicator
+      accessibilityLabel={accessibilityLabel}
+      color={color}
+      testID={testID}
+    />
+  );
+};
 
-  render() {
-    const color = this.props.element.getAttribute('color') || '#8d9494';
-    const { testID, accessibilityLabel } = createTestProps(this.props.element);
-    return (
-      <ActivityIndicator
-        accessibilityLabel={accessibilityLabel}
-        color={color}
-        testID={testID}
-      />
-    );
-  }
-}
+HvSpinner.namespaceURI = Namespaces.HYPERVIEW;
+HvSpinner.localName = LOCAL_NAME.SPINNER;
+
+export default HvSpinner;
