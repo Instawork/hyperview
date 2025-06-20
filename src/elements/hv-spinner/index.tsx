bf37226b7e5983da06fa89ba2fdcb186ab097738
Hardin Gray <hgray@instawork.com>
2025-06-20 15:04:54 -0400

    chore: memoize/cleanup

--- src/elements/hv-spinner/index.tsx
+++ src/elements/hv-spinner/index.tsx
@@ -1,13 +1,18 @@
 import * as Namespaces from 'hyperview/src/services/namespaces';
+import React, { useMemo } from 'react';
 import { ActivityIndicator } from 'react-native';
 import type { HvComponentProps } from 'hyperview/src/types';
 import { LOCAL_NAME } from 'hyperview/src/types';
-import React from 'react';
 import { createTestProps } from 'hyperview/src/services';
 
 const HvSpinner = (props: HvComponentProps) => {
-  const color = props.element.getAttribute('color') || '#8d9494';
-  const { testID, accessibilityLabel } = createTestProps(props.element);
+  // eslint-disable-next-line react/destructuring-assignment
+  const { element } = props;
+
+  const color = useMemo(() => element.getAttribute('color') || '#8d9494', [
+    element,
+  ]);
+  const { testID, accessibilityLabel } = createTestProps(element);
 
   return (
     <ActivityIndicator
