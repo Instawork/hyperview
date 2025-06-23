1748d6cd675dab04cf508c2ae40b8b00fedf8fc8
Hardin Gray <hgray@instawork.com>
2025-06-23 12:59:17 -0400

    chore: improve memoization

--- src/elements/hv-picker-field/index.tsx
+++ src/elements/hv-picker-field/index.tsx
@@ -5,6 +5,7 @@ import type {
   HvComponentProps,
   StyleSheet,
 } from 'hyperview/src/types';
+import React, { useCallback } from 'react';
 import {
   createStyleProp,
   createTestProps,
@@ -12,7 +13,6 @@ import {
 } from 'hyperview/src/services';
 import { LOCAL_NAME } from 'hyperview/src/types';
 import Picker from 'hyperview/src/core/components/picker';
-import React from 'react';
 import { View } from 'react-native';
 
 /**
@@ -22,103 +22,110 @@ import { View } from 'react-native';
  *   opens a system dialog.
  */
 const HvPickerField = (props: HvComponentProps) => {
+  // eslint-disable-next-line react/destructuring-assignment
+  const { element, onUpdate, options, stylesheets } = props;
+
   /**
    * Returns a string representing the value in the field.
    */
-  const getValue = (): string => props.element.getAttribute('value') || '';
+  const getValue = useCallback(
+    (): string => element.getAttribute('value') || '',
+    [element],
+  );
 
   /**
    * Returns a string representing the value in the picker.
    */
-  const getPickerValue = (): string =>
-    props.element.getAttribute('value') || '';
+  const getPickerValue = useCallback(
+    (): string => element.getAttribute('value') || '',
+    [element],
+  );
 
-  const getPickerItems = (): Element[] =>
-    Array.from(
-      props.element.getElementsByTagNameNS(
-        Namespaces.HYPERVIEW,
-        LOCAL_NAME.PICKER_ITEM,
+  const getPickerItems = useCallback(
+    (): Element[] =>
+      Array.from(
+        element.getElementsByTagNameNS(
+          Namespaces.HYPERVIEW,
+          LOCAL_NAME.PICKER_ITEM,
+        ),
       ),
-    );
+    [element],
+  );
 
-  const onFocus = () => {
-    const newElement = props.element.cloneNode(true) as Element;
+  const onFocus = useCallback(() => {
+    const newElement = element.cloneNode(true) as Element;
     newElement.setAttribute('focused', 'true');
-    props.onUpdate(null, 'swap', props.element, { newElement });
-    Behaviors.trigger('focus', newElement, props.onUpdate);
-  };
+    onUpdate(null, 'swap', element, { newElement });
+    Behaviors.trigger('focus', newElement, onUpdate);
+  }, [element, onUpdate]);
 
-  const onBlur = () => {
-    const newElement = props.element.cloneNode(true) as Element;
+  const onBlur = useCallback(() => {
+    const newElement = element.cloneNode(true) as Element;
     newElement.setAttribute('focused', 'false');
-    props.onUpdate(null, 'swap', props.element, { newElement });
-    Behaviors.trigger('blur', newElement, props.onUpdate);
-  };
+    onUpdate(null, 'swap', element, { newElement });
+    Behaviors.trigger('blur', newElement, onUpdate);
+  }, [element, onUpdate]);
 
   /**
    * Hides the picker without applying the chosen value.
    */
-  const onCancel = () => {
-    const newElement = props.element.cloneNode(true) as Element;
+  const onCancel = useCallback(() => {
+    const newElement = element.cloneNode(true) as Element;
     newElement.setAttribute('focused', 'false');
     newElement.removeAttribute('picker-value');
-    props.onUpdate(null, 'swap', props.element, { newElement });
-  };
+    onUpdate(null, 'swap', element, { newElement });
+  }, [element, onUpdate]);
 
   /**
    * Hides the picker and applies the chosen value to the field.
    */
-  const onDone = (newValue?: string) => {
-    const pickerValue = newValue !== undefined ? newValue : getPickerValue();
-    const value = getValue();
-    const newElement = props.element.cloneNode(true) as Element;
-    newElement.setAttribute('value', pickerValue);
-    newElement.removeAttribute('picker-value');
-    newElement.setAttribute('focused', 'false');
-    props.onUpdate(null, 'swap', props.element, { newElement });
+  const onDone = useCallback(
+    (newValue?: string) => {
+      const pickerValue = newValue !== undefined ? newValue : getPickerValue();
+      const value = getValue();
+      const newElement = element.cloneNode(true) as Element;
+      newElement.setAttribute('value', pickerValue);
+      newElement.removeAttribute('picker-value');
+      newElement.setAttribute('focused', 'false');
+      onUpdate(null, 'swap', element, { newElement });
 
-    const hasChanged = value !== pickerValue;
-    if (hasChanged) {
-      Behaviors.trigger('change', newElement, props.onUpdate);
-    }
-  };
-
-  const onChange = (value: string | null | undefined) => {
-    if (value === undefined) {
-      onCancel();
-    } else {
-      onDone(value || '');
-    }
-  };
-
-  const style: Array<StyleSheet> = createStyleProp(
-    props.element,
-    props.stylesheets,
-    {
-      ...props.options,
-      styleAttr: 'field-text-style',
+      const hasChanged = value !== pickerValue;
+      if (hasChanged) {
+        Behaviors.trigger('change', newElement, onUpdate);
+      }
     },
+    [element, getPickerValue, getValue, onUpdate],
   );
-  const { testID, accessibilityLabel } = createTestProps(props.element);
-  const value: DOMString | null | undefined = props.element.getAttribute(
-    'value',
+
+  const onChange = useCallback(
+    (value: string | null | undefined) => {
+      if (value === undefined) {
+        onCancel();
+      } else {
+        onDone(value || '');
+      }
+    },
+    [onCancel, onDone],
   );
+
+  const style: Array<StyleSheet> = createStyleProp(element, stylesheets, {
+    ...options,
+    styleAttr: 'field-text-style',
+  });
+  const { testID, accessibilityLabel } = createTestProps(element);
+  const value: DOMString | null | undefined = element.getAttribute('value');
   const placeholderTextColor:
     | DOMString
     | null
-    | undefined = props.element.getAttribute('placeholderTextColor');
+    | undefined = element.getAttribute('placeholderTextColor');
   if ([undefined, null, ''].includes(value) && placeholderTextColor) {
     style.push({ color: placeholderTextColor });
   }
 
-  const fieldStyle: Array<StyleSheet> = createStyleProp(
-    props.element,
-    props.stylesheets,
-    {
-      ...props.options,
-      styleAttr: 'field-style',
-    },
-  );
+  const fieldStyle: Array<StyleSheet> = createStyleProp(element, stylesheets, {
+    ...options,
+    styleAttr: 'field-style',
+  });
 
   // Gets all of the <picker-item> elements. All picker item elements
   // with a value and label are turned into options for the picker.
@@ -150,8 +157,8 @@ const HvPickerField = (props: HvComponentProps) => {
         // eslint-disable-next-line max-len
         // `enabled` needs to be true when the field is not focused, otherwise the the field will not be selectable
         // fix inspired by https://github.com/react-native-picker/picker/issues/95#issuecomment-935718568
-        enabled={props.element.getAttribute('focused') !== 'true'}
-        label={props.element.getAttribute('placeholder') || undefined}
+        enabled={element.getAttribute('focused') !== 'true'}
+        label={element.getAttribute('placeholder') || undefined}
         style={{ fontSize: 16 }}
         value=""
       />,
