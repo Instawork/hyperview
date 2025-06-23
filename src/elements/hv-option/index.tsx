1d9895484adf3387ec19a7d52150fa7353ff7e0b
Hardin Gray <hgray@instawork.com>
2025-06-23 13:35:45 -0400

    chore(option): convert to Functional Component (#1195)
    Convert `hv-option` to Functional Component. Confirm no degradation in
    render count.
    
    
    [Asana](https://app.asana.com/1/47184964732898/project/1204008699308084/task/1210601789543015?focus=true)

--- src/elements/hv-option/index.tsx
+++ src/elements/hv-option/index.tsx
@@ -5,10 +5,9 @@ import type {
   HvComponentOnUpdate,
   HvComponentProps,
 } from 'hyperview/src/types';
-import React, { PureComponent } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { TouchableWithoutFeedback, View } from 'react-native';
 import { LOCAL_NAME } from 'hyperview/src/types';
-import type { State } from './types';
 import { createEventHandler } from 'hyperview/src/core/utils';
 import { createProps } from 'hyperview/src/services';
 
@@ -16,86 +15,82 @@ import { createProps } from 'hyperview/src/services';
  * A component representing an option in a single-select or multiple-select list.
  * Has a local pressed state. The selected state is read from the element attribute.
  */
-export default class HvOption extends PureComponent<HvComponentProps, State> {
-  static namespaceURI = Namespaces.HYPERVIEW;
+const HvOption = (props: HvComponentProps) => {
+  // eslint-disable-next-line react/destructuring-assignment
+  const { element, onUpdate, options, stylesheets } = props;
+  const { onSelect, onToggle } = options;
+  const [pressed, setPressed] = useState(false);
+  const prevProps = useRef<HvComponentProps>(props);
 
-  static localName = LOCAL_NAME.OPTION;
+  const value = element.getAttribute('value');
+  const selected = element.getAttribute('selected') === 'true';
 
-  state: State = {
-    pressed: false,
+  // Updates options with pressed/selected state, so that child element can render
+  // using the appropriate modifier styles.
+  const newOptions = {
+    ...options,
+    pressed,
+    pressedSelected: pressed && selected,
+    selected,
+  } as const;
+  const componentProps = createProps(element, stylesheets, newOptions);
+
+  // Option renders as an outer TouchableWithoutFeedback view and inner view.
+  // The outer view handles presses, the inner view handles styling.
+  const outerProps = {
+    onPress: createEventHandler(() => {
+      if (onSelect) {
+        // Updates the DOM state, causing this element to re-render as selected.
+        // Used in select-single context.
+        onSelect(value);
+      }
+      if (onToggle) {
+        // Updates the DOM state, toggling this element.
+        // Used in select-multiple context.
+        onToggle(value);
+      }
+    }, true),
+    onPressIn: createEventHandler(() => setPressed(true)),
+    onPressOut: createEventHandler(() => setPressed(false)),
+    style: {},
   };
+  if (componentProps.style && componentProps.style.flex) {
+    // Flex is a style that needs to be lifted from the inner component to the outer
+    // component to ensure proper layout.
+    outerProps.style = { flex: componentProps.style.flex };
+  }
 
-  componentDidUpdate(prevProps: HvComponentProps) {
-    const selected = this.props.element.getAttribute('selected') === 'true';
-    const prevSelected = prevProps.element.getAttribute('selected') === 'true';
+  useEffect(() => {
+    const prevSelected =
+      prevProps.current?.element.getAttribute('selected') === 'true';
     if (selected && !prevSelected) {
-      Behaviors.trigger('select', this.props.element, this.props.onUpdate);
+      Behaviors.trigger('select', element, onUpdate);
     }
 
     if (!selected && prevSelected) {
-      Behaviors.trigger('deselect', this.props.element, this.props.onUpdate);
+      Behaviors.trigger('deselect', element, onUpdate);
     }
-  }
+    prevProps.current = props;
+  }, [element, onUpdate, props, selected]);
 
-  render() {
-    const { onSelect, onToggle } = this.props.options;
-
-    const value = this.props.element.getAttribute('value');
-    const selected = this.props.element.getAttribute('selected') === 'true';
-
-    // Updates options with pressed/selected state, so that child element can render
-    // using the appropriate modifier styles.
-    const newOptions = {
-      ...this.props.options,
-      pressed: this.state.pressed,
-      pressedSelected: this.state.pressed && selected,
-      selected,
-    } as const;
-    const props = createProps(
-      this.props.element,
-      this.props.stylesheets,
-      newOptions,
-    );
-
-    // Option renders as an outer TouchableWithoutFeedback view and inner view.
-    // The outer view handles presses, the inner view handles styling.
-    const outerProps = {
-      onPress: createEventHandler(() => {
-        if (onSelect) {
-          // Updates the DOM state, causing this element to re-render as selected.
-          // Used in select-single context.
-          onSelect(value);
-        }
-        if (onToggle) {
-          // Updates the DOM state, toggling this element.
-          // Used in select-multiple context.
-          onToggle(value);
-        }
-      }, true),
-      onPressIn: createEventHandler(() => this.setState({ pressed: true })),
-      onPressOut: createEventHandler(() => this.setState({ pressed: false })),
-      style: {},
-    };
-    if (props.style && props.style.flex) {
-      // Flex is a style that needs to be lifted from the inner component to the outer
-      // component to ensure proper layout.
-      outerProps.style = { flex: props.style.flex };
-    }
-
-    // TODO: Replace with <HvChildren>
-    return React.createElement(
-      TouchableWithoutFeedback,
-      outerProps,
-      React.createElement(
-        View,
-        props,
-        ...Render.renderChildren(
-          this.props.element,
-          this.props.stylesheets,
-          this.props.onUpdate as HvComponentOnUpdate,
-          newOptions,
-        ),
+  // TODO: Replace with <HvChildren>
+  return React.createElement(
+    TouchableWithoutFeedback,
+    outerProps,
+    React.createElement(
+      View,
+      componentProps,
+      ...Render.renderChildren(
+        element,
+        stylesheets,
+        onUpdate as HvComponentOnUpdate,
+        newOptions,
       ),
-    );
-  }
-}
+    ),
+  );
+};
+
+HvOption.namespaceURI = Namespaces.HYPERVIEW;
+HvOption.localName = LOCAL_NAME.OPTION;
+
+export default HvOption;
