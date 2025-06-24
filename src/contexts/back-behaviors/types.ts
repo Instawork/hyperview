105a8ab1066ea26c5c523ce6b945642fb96fbbd9
Hardin Gray <hgray@instawork.com>
2025-06-24 11:49:16 -0400

    chore: create context

---
+++ src/contexts/back-behaviors/types.ts
@@ -0,0 +1,8 @@
+import { HvComponentOnUpdate } from 'hyperview/src/types';
+
+export type Props = {
+  add: (elements: Element[], onUpdate: HvComponentOnUpdate) => void;
+  get: () => Element[];
+  onUpdate: HvComponentOnUpdate;
+  remove: (elements: Element[]) => void;
+};
