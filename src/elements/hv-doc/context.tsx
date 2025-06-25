b1b2194a7c8c31a7ffd1e18c773c7c4003445078
Hardin Gray <hgray@instawork.com>
2025-06-24 23:51:47 -0400

    chore(doc-context): improve context implementation (#1211)
    Improve the implementation of the context using the `Consumer` pattern.
    
    
    [Asana](https://app.asana.com/1/47184964732898/project/1204008699308084/task/1210620686213505?focus=true)

---
+++ src/elements/hv-doc/context.tsx
@@ -0,0 +1,25 @@
+import React, { ConsumerProps, createContext, useContext } from 'react';
+import { ContextProps } from './types';
+
+export const Context = createContext<ContextProps | undefined>(undefined);
+
+export const useHvDocContext = () => {
+  const context = useContext(Context);
+  if (!context) {
+    throw new Error('Trying to use HvDoc context without provider');
+  }
+  return context;
+};
+
+export const Consumer = (props: ConsumerProps<ContextProps>) => {
+  return (
+    <Context.Consumer>
+      {context => {
+        if (!context) {
+          throw new Error('Trying to use HvDoc context without provider');
+        }
+        return props.children(context);
+      }}
+    </Context.Consumer>
+  );
+};
