f93d7ad3c10e7c701157d1b8a15c8abfe48cb16f
Hardin Gray <hgray@instawork.com>
2025-06-26 20:37:16 -0400

    chore: remove sub-components (#1219)
    To improve render performance, replace instances of sub-components with
    direct implementation in the return value.
    
    
    [Asana](https://app.asana.com/1/47184964732898/project/1204008699308084/task/1210645183157618?focus=true)

--- src/elements/hv-image/index.tsx
+++ src/elements/hv-image/index.tsx
@@ -1,26 +1,27 @@
 import * as Namespaces from 'hyperview/src/services/namespaces';
-import React, { useMemo } from 'react';
+import { Image, ImageProps } from 'react-native';
 import type { HvComponentProps } from 'hyperview/src/types';
-import { Image } from 'react-native';
 import { LOCAL_NAME } from 'hyperview/src/types';
+import React from 'react';
 import { createProps } from 'hyperview/src/services';
 import urlParse from 'url-parse';
 
 const HvImage = (props: HvComponentProps) => {
   // eslint-disable-next-line react/destructuring-assignment
-  const { element, options, stylesheets } = props;
+  const { options } = props;
   const { screenUrl } = options;
-  const componentProps = useMemo(() => {
-    const baseProps = createProps(element, stylesheets, options);
-    const source = element.getAttribute('source');
-    if (!source) {
-      return baseProps;
-    }
-    return {
-      ...baseProps,
-      source: { uri: urlParse(source, screenUrl, true).toString() },
-    };
-  }, [element, screenUrl, stylesheets, options]);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const imageProps: Record<string, any> = {};
+  let source = props.element.getAttribute('source');
+  if (source) {
+    source = urlParse(source, screenUrl, true).toString();
+    imageProps.source = { uri: source };
+  }
+  const componentProps = {
+    ...createProps(props.element, props.stylesheets, props.options),
+    ...imageProps,
+  } as ImageProps;
 
   return React.createElement(Image, componentProps);
 };
--- src/elements/hv-text/index.tsx
+++ src/elements/hv-text/index.tsx
@@ -4,35 +4,30 @@ import type {
   HvComponentOnUpdate,
   HvComponentProps,
 } from 'hyperview/src/types';
-import React, { useCallback } from 'react';
 import { LOCAL_NAME } from 'hyperview/src/types';
+import React from 'react';
 import { Text } from 'react-native';
 import { createProps } from 'hyperview/src/services';
 
 const HvText = (props: HvComponentProps) => {
   // eslint-disable-next-line react/destructuring-assignment
   const { element, onUpdate, options, stylesheets } = props;
+  const componentProps = createProps(element, stylesheets, options);
 
-  const Component = useCallback(() => {
-    const componentProps = createProps(element, stylesheets, options);
-
-    // TODO: Replace with <HvChildren>
-    return React.createElement(
-      Text,
-      componentProps,
-      ...Render.renderChildren(
-        element,
-        stylesheets,
-        onUpdate as HvComponentOnUpdate,
-        {
-          ...options,
-          preformatted: element.getAttribute('preformatted') === 'true',
-        },
-      ),
-    );
-  }, [element, onUpdate, options, stylesheets]);
-
-  return <Component />;
+  // TODO: Replace with <HvChildren>
+  return React.createElement(
+    Text,
+    componentProps,
+    ...Render.renderChildren(
+      element,
+      stylesheets,
+      onUpdate as HvComponentOnUpdate,
+      {
+        ...options,
+        preformatted: element.getAttribute('preformatted') === 'true',
+      },
+    ),
+  );
 };
 
 HvText.namespaceURI = Namespaces.HYPERVIEW;
