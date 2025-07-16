77038781394b7986e9e71b1fe110dac41b36e32b
flochtililoch <flochtililoch@gmail.com>
2025-07-16 15:02:23 -0400

    fix: schema deploy

--- website/build-schema.sh
+++ website/build-schema.sh
@@ -2,5 +2,5 @@
 
 mkdir build/Hyperview/schema
 mkdir build/Hyperview/schema/new
-cp ../schema/*.xsd build/Hyperview/schema
-cp ../schema/*.xsd build/Hyperview/schema/new
+cp ../hyperview/schema/*.xsd build/Hyperview/schema
+cp ../hyperview/schema/*.xsd build/Hyperview/schema/new
