0ccfbc81e72a87c4aff87654a6e3fce0813fc9c0
Hardin Gray <hgray@instawork.com>
2025-09-25 17:21:59 -0400

    chore: remove legacy bridge

--- src/core/hyper-ref/index.ts
+++
@@ -1,5 +0,0 @@
-import { createEventHandler } from 'hyperview/src/services';
-
-// TODO: Remove this once we have a proper way to import the createEventHandler
-// function from external files
-export { createEventHandler };
