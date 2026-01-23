e2b72d75164d9b1f04342e5836d8a9eca941c6d6
Hardin Gray <hgray@instawork.com>
2026-01-23 09:55:38 -0500

    chore: move base error

---
+++ src/services/error/hv-base-error.ts
@@ -0,0 +1,45 @@
+import type { ExtraContext } from './types';
+
+export class HvBaseError extends Error {
+  extraContext: ExtraContext = {};
+
+  name = 'HvBaseError';
+
+  constructor(message: string) {
+    super(message);
+    if (this.constructor === HvBaseError) {
+      throw new Error('Do not instantiate `HvBaseError` directly');
+    }
+  }
+
+  castExtraContextValue = (value: unknown): string => {
+    switch (typeof value) {
+      case 'string':
+        return value;
+      case 'function':
+        return value.name || 'anonymous function';
+      case 'boolean':
+      case 'undefined':
+      case 'number':
+        return String(value);
+      case 'object':
+      default:
+        return JSON.stringify(value);
+    }
+  };
+
+  setExtraContext: (key: string, value: unknown) => void = (
+    key: string,
+    value: unknown,
+  ) => {
+    this.extraContext[key] = this.castExtraContextValue(value);
+  };
+
+  getExtraContext(): ExtraContext {
+    return this.extraContext;
+  }
+
+  getFingerprint(): string[] {
+    return [this.name, this.message];
+  }
+}
---
+++ src/services/error/types.ts
@@ -0,0 +1,1 @@
+export type ExtraContext = { [key: string]: unknown };
