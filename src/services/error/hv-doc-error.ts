fac869fd564fd65535eaa5f60bad7f7dbf7bdf84
Hardin Gray <hgray@instawork.com>
2026-01-26 13:52:10 -0500

    chore: update element/doc errors

--- src/services/error/hv-doc-error.ts
+++ src/services/error/hv-doc-error.ts
@@ -1,13 +1,30 @@
+import { ExtraContext } from './types';
 import { HvBaseError } from './hv-base-error';
+import { XMLSerializer } from '@instawork/xmldom';
 
 export class HvDocError extends HvBaseError {
   name = 'HvDocError';
 
+  doc: Document;
+
+  docString: string | null = null;
+
   constructor(message: string, doc: Document) {
     super(message);
-    this.setExtraContext('doc', doc);
+    this.doc = doc;
     if (this.constructor === HvDocError) {
       throw new Error('Do not instantiate `HvDocError` directly');
     }
+  }
+
+  getExtraContext(): ExtraContext {
+    if (!this.docString) {
+      const serializer = new XMLSerializer();
+      this.docString = serializer.serializeToString(this.doc);
+    }
+    return {
+      ...super.getExtraContext(),
+      doc: this.docString,
+    };
   }
 }
--- src/services/error/hv-element-error.ts
+++ src/services/error/hv-element-error.ts
@@ -1,13 +1,41 @@
+import { ExtraContext } from './types';
 import { HvBaseError } from './hv-base-error';
+import { XMLSerializer } from '@instawork/xmldom';
 
 export class HvElementError extends HvBaseError {
   name = 'HvElementError';
 
+  element: Element;
+
+  elementString: string | null = null;
+
+  parentString: string | null = null;
+
   constructor(message: string, element: Element) {
     super(message);
-    this.setExtraContext('element', element);
+    this.element = element;
     if (this.constructor === HvElementError) {
       throw new Error('Do not instantiate `HvElementError` directly');
     }
+  }
+
+  getExtraContext(): ExtraContext {
+    if (!this.elementString || !this.parentString) {
+      const serializer = new XMLSerializer();
+      this.elementString = serializer.serializeToString(this.element);
+      if (this.element.parentNode) {
+        this.parentString = serializer.serializeToString(
+          this.element.parentNode as Element,
+        );
+      } else {
+        this.parentString = '';
+      }
+    }
+
+    return {
+      ...super.getExtraContext(),
+      element: this.elementString,
+      parent: this.parentString,
+    };
   }
 }
