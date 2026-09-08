be29d973295c70d3216b3232adcfadb1025d390e
Hardin Gray <hgray@instawork.com>
2026-09-08 13:41:18 -0400

    wip: updated templates

---
+++ demo/backend/_data/manyTabs.js
@@ -0,0 +1,12 @@
+module.exports = [
+  { icon: 'ui', id: 1, label: 'Home' },
+  { icon: 'behaviors', id: 2, label: 'Star' },
+  { icon: 'navigation', id: 3, label: 'Heart' },
+  { icon: 'advanced', id: 4, label: 'Alerts' },
+  { icon: 'ui', id: 5, label: 'Settings' },
+  { icon: 'behaviors', id: 6, label: 'Profile' },
+  { icon: 'navigation', id: 7, label: 'Saved' },
+  { icon: 'advanced', id: 8, label: 'Mail' },
+  { icon: 'ui', id: 9, label: 'Files' },
+  { icon: 'behaviors', id: 10, label: 'Map' },
+];
